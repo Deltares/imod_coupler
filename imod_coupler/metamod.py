b6c9ab86000502ebf1b7a6edb6a7bed260a29b8f
Julian Hofer <julian.git@mailbox.org>
2020-06-22 17:46:31 +0200

    Add entry point, rename and refactor

--- imod_coupler/metamod.py
+++ imod_coupler/metamod.py
@@ -4,7 +4,7 @@ import os
 import json
 import numpy as np
 from amipy import AmiWrapper
-import argparse
+import logging
 
 
 def mapids(ids1, ids2):
@@ -161,7 +161,7 @@ class MetaMod(AmiWrapper):
             while kiter < self.max_iter:
                 has_converged = self.do_iter(sol_id)
                 if has_converged:
-                    print(
+                    logging.info(
                         "\n\nComponent ",
                         sol_id,
                         " converged in ",
@@ -229,126 +229,3 @@ class MetaMod(AmiWrapper):
         # Initalize the heads in MetaSWAP by copying them from MODFLOW
         self.xchg_mod2msw()
 
-
-# copied from https://stackoverflow.com/a/10551190
-class EnvDefault(argparse.Action):
-    def __init__(self, envvar, required=True, default=None, **kwargs):
-        if not default and envvar:
-            if envvar in os.environ:
-                default = os.environ[envvar]
-        if required and default:
-            required = False
-        super(EnvDefault, self).__init__(default=default, required=required, **kwargs)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, values)
-
-
-def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--mf6-dll",
-        required=True,
-        action=EnvDefault,
-        envvar="MF6_DLL",
-        help="Specify the path to Modflow6 dll \
-            (can also be specified using MF6_DLL environment variable)",
-    )
-    parser.add_argument(
-        "--msw-dll",
-        required=True,
-        action=EnvDefault,
-        envvar="MSW_DLL",
-        help="Specify the path to Metaswap dll \
-            (can also be specified using MSW_DLL environment variable)",
-    )
-    parser.add_argument(
-        "--mf6-model-dir",
-        required=True,
-        action=EnvDefault,
-        envvar="MF6_MODEL_DIR",
-        help="Specify the path to Modflow6 model directory "
-        + "(can also be specified using MF6_MODEL_DIR environment variable)",
-    )
-    parser.add_argument(
-        "--msw-model-dir",
-        required=True,
-        action=EnvDefault,
-        envvar="MSW_MODEL_DIR",
-        help="Specify the path to Metaswap model directory "
-        + "(can also be specified using MSW_MODEL_DIR environment variable)",
-    )
-    # Remove this argument, as soon as the metaswap dll's are in the right place again
-    parser.add_argument(
-        "--msw-mpi-dll-dir",
-        action=EnvDefault,
-        envvar="MSW_MPI_DLL_DIR",
-        help="Specify the path containing the Metaswap MPI dlls "
-        + "(can also be specified using MSW_MPI_DLL_DIR environment variable)",
-    )
-
-    # Remove this argument, as soon as the metaswap dll's are in the right place again
-    parser.add_argument(
-        "--enable-debug-native",
-        action="store_true",
-        help="Stop the script to wait for the native debugger",
-    )
-
-    args = parser.parse_args()
-
-    mf6_dll = args.mf6_dll
-    msw_dll = args.msw_dll
-    mf6_model_dir = args.mf6_model_dir
-    msw_model_dir = args.msw_model_dir
-    msw_mpi_dll_dir = args.msw_mpi_dll_dir
-    debug_native = args.enable_debug_native
-
-    if not os.path.exists(mf6_dll):
-        sys.stderr.write("MODFLOW6 dll " + mf6_dll + " not found.")
-        sys.exit()
-
-    if not os.path.exists(msw_dll):
-        sys.stderr.write("METASWAP dd " + msw_dll + " not found.")
-        sys.exit()
-
-    if not os.path.isdir(mf6_model_dir):
-        sys.stderr.write("MODFLOW6 Model path " + mf6_model_dir + " not found.")
-        sys.exit()
-
-    if not os.path.isdir(msw_model_dir):
-        sys.stderr.write("MetaSWAP Model path " + msw_model_dir + " not found.")
-        sys.exit()
-
-    if os.path.isdir(msw_mpi_dll_dir):
-        os.environ["PATH"] += os.pathsep + msw_mpi_dll_dir
-    else:
-        sys.stderr.write("Metaswap MPI dlls " + msw_mpi_dll_dir + " not found.")
-        sys.exit()
-
-    # for debugging
-    if debug_native:
-        print("PID: ", os.getpid(), "; continue? [y]")
-        answer = input()
-        if answer != "y":
-            exit(0)
-
-    # Create an instance
-    MMinst = MetaMod(
-        mf6_modeldir=mf6_model_dir,
-        msw_modeldir=msw_model_dir,
-        mf6_dll=mf6_dll,
-        msw_dll=msw_dll,
-    )
-    # Run the time loop
-    start_time, current_time, end_time = MMinst.getTimes()
-
-    while current_time < end_time:
-        current_time = MMinst.do_time_step()
-    MMinst.finalize()
-    sys.stderr.write("NEW SIMULATION TERMINATED NORMALLY")
-
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()
--- setup.py
+++ setup.py
@@ -2,25 +2,26 @@ import sys
 from setuptools import setup
 
 __version__ = "0.1.0"
-__name__ = "amipy"
+__name__ = "imod_coupler"
 __author__ = "Deltares"
 
 # ensure minimum version of Python is running
 if sys.version_info[0:2] < (3, 6):
-    raise RuntimeError("imod-coupler requires Python >= 3.6")
+    raise RuntimeError("imod_coupler requires Python >= 3.6")
 
 
 setup(
     name=__name__,
-    description="The imod-coupler can be usedto couple hydrologic kernels.",
+    description="The imod_coupler can be used to couple hydrologic kernels.",
     author=__author__,
     author_email="",
     url="https://github.com/Deltares/imod-coupler",
     license="AGPL",
     platforms="Windows, Mac OS-X, Linux",
-    install_requires=["amipy"],
+    install_requires=["numpy"],
     packages=[__name__],
     include_package_data=True,
     version=__version__,
     classifiers=["Topic :: Scientific/Engineering :: Hydrology"],
+    entry_points={"console_scripts": ["imodc = imod_coupler.__main__:main"]},
 )
