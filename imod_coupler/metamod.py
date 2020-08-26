10d9d82d6504db8740caed6b60eeb75c01eadb9c
Martijn Russcher <Martijn.Russcher@deltares.nl>
2020-08-26 15:18:19 +0100

    flake8

--- imod_coupler/metamod.py
+++ imod_coupler/metamod.py
@@ -136,7 +136,7 @@ class MetaMod:
         mf6_max_iter_tag = self.mf6.get_var_address("MXITER", "SLN_1")
 
         self.mf6_head = self.mf6.get_value_ptr(mf6_head_tag)
-        self.mf6_recharge = self.mf6.get_value_ptr(mf6_recharge_tag)[:,0] # recharge is the first column in the MODFLOW BOUND array
+        self.mf6_recharge = self.mf6.get_value_ptr(mf6_recharge_tag)[:, 0]  # set to first column in BOUND
         self.mf6_storage = self.mf6.get_value_ptr(mf6_storage_tag)
         self.mf6_sto_reset = self.mf6.get_value_ptr(mf6_sto_reset_tag)
         self.max_iter = self.mf6.get_value_ptr(mf6_max_iter_tag)[0]
