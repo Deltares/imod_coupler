b6e50f155e819c8736c1dcca8486e8599cca10ab
Hofer-Julian <julianhofer@gnome.org>
2022-03-17 17:11:49 +0100

    Start with move to drivers

--- imod_coupler/__main__.py
+++ imod_coupler/__main__.py
@@ -1,13 +1,13 @@
 import argparse
 import logging
 import os
-import sys
 import time
+from pathlib import Path
+
+import toml
 
 from imod_coupler import __version__
-from imod_coupler.config import Config
 from imod_coupler.drivers.metamod.metamod import MetaMod
-from imod_coupler.errors import ConfigError
 
 logger = logging.getLogger(__name__)
 
@@ -19,20 +19,6 @@ def main():
         "config_path",
         action="store",
         help="specify the path to the configuration file",
-    )
-
-    parser.add_argument(
-        "--log-level",
-        action="store",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        default="INFO",
-        help="define log level",
-    )
-
-    parser.add_argument(
-        "--timing",
-        action="store_true",
-        help="activate timing (verbosity can be adjusted with the log-level)",
     )
 
     parser.add_argument(
@@ -48,55 +34,37 @@ def main():
     )
 
     args = parser.parse_args()
-    config_path = args.config_path
-    logging.basicConfig(level=args.log_level)
-    timing = args.timing
-    debug_native = args.enable_debug_native
+    config_path = Path(args.config_path).resolve()
+    config = toml.load(config_path)
 
-    if timing:
+    # TODO: validate with generic json schema
+
+    logging.basicConfig(level=config["log_level"])
+
+    if config["timing"]:
         start = time.perf_counter()
 
-    try:
-        config = Config(config_path, timing)
-    except ConfigError as e:
-        logger.error("Could not parse configuration file")
-        logger.error(e)
-        sys.exit(1)
-
-    if debug_native:
+    if args.enable_debug_native:
         # wait for native debugging
         input(f"PID: {os.getpid()}, press any key to continue ....")
 
-    kernels = config.kernels
-    for exchange in config.exchanges:
-        if "modflow6" in exchange["kernels"] and "metaswap" in exchange["kernels"]:
-            mf6 = kernels["modflow6"]
-            msw = kernels["metaswap"]
+    if config["driver_type"] == "metamod":
+        driver = MetaMod(config, config_path)
 
-            # Print output to stdout
-            mf6.set_int("ISTDOUTTOFILE", 0)
+    # This will initialize and couple the kernels
+    driver.initialize()
 
-            # Create an instance
-            metamod = MetaMod(mf6=mf6, msw=msw, timing=timing)
+    # Run the time loop
+    start_time, current_time, end_time = driver.get_times()
+    while current_time < end_time:
+        current_time = driver.update()
+    logger.info("New simulation terminated normally")
 
-            # This will initialize and couple the kernels
-            metamod.initialize()
-
-            # Run the time loop
-            start_time, current_time, end_time = metamod.get_times()
-            while current_time < end_time:
-                current_time = metamod.update()
-            logger.info("New Simulation terminated normally")
-
-            metamod.finalize()
+    driver.finalize()
 
     # Report timing
-    if timing:
-        total = 0
-        for kernel in kernels.values():
-            total += kernel.report_timing_totals()
-        logger.info(f"Total elapsed time in numerical kernels: {total:0.4f} seconds")
-
+    if config["timing"]:
+        driver.report_timing_totals()
         end = time.perf_counter()
         logger.info(f"Total elapsed time: {end-start:0.4f} seconds")
 
