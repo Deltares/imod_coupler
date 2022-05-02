7d78bb90d6681ba2939e4966b7f90d96df138b76
Hofer-Julian <julianhofer@gnome.org>
2022-05-02 13:58:45 +0200

    Add tests for DLLs

--- tests/test_general.py
+++ tests/test_general.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from imod_coupler import __version__
 
 
-def test_get_version(imodc_path: Path) -> None:
+def test_get_version(imodc: Path) -> None:
     output_version = subprocess.run(
-        [imodc_path, "--version"], capture_output=True, text=True
+        [imodc, "--version"], capture_output=True, text=True
     ).stdout.strip("\n")
 
-    expected_version = f"{imodc_path.name} {__version__}"
+    expected_version = f"{imodc.name} {__version__}"
 
     assert output_version == expected_version
