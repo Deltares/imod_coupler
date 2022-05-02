b55078925f02a762edb9c41d9dab4a53b1fb5fb6
Hofer-Julian <julianhofer@gnome.org>
2022-05-02 10:20:04 +0200

    Add test to check version output

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,15 @@ import pytest
 from dotenv import load_dotenv
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def database_path() -> Path:
     load_dotenv()
     database_path = Path(os.environ["METASWAP_LOOKUP_TABLE"])
     return database_path
+
+
+@pytest.fixture(scope="session")
+def imodc_path() -> Path:
+    load_dotenv()
+    imodc_path = Path(os.environ["IMOD_COUPLER_EXECUTABLE"])
+    return imodc_path
