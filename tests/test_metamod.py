36919cb867b021f4e0152dd9431bf26528238b00
Hofer-Julian <julianhofer@gnome.org>
2022-05-06 13:49:47 +0200

    Minor test fixes

--- tests/test_metamod.py
+++ tests/test_metamod.py
@@ -10,14 +10,37 @@ def test_metaswap_dll_devel_present(modflow_dll_devel: Path) -> None:
     assert modflow_dll_devel.is_file()
 
 
-def test_modflow_dll_release_present(modflow_dll_regression: Path) -> None:
+def test_metaswap_dll_devel_present(modflow_dll_devel: Path) -> None:
+    assert modflow_dll_devel.is_file()
+
+
+def test_modflow_dll_devel_present(modflow_dll_devel: Path) -> None:
+    assert modflow_dll_devel.is_file()
+
+
+def test_modflow_dll_regression_present(modflow_dll_regression: Path) -> None:
     assert modflow_dll_regression.is_file()
 
 
-def test_metaswap_dll_dep_dir_contains_dependencies(
+def test_metaswap_dll_dep_dir_devel_contains_dependencies(
     metaswap_dll_dep_dir_devel: Path,
 ) -> None:
     dep_dir_content = os.listdir(metaswap_dll_dep_dir_devel)
+    expected_dependencies = (
+        "fmpich2.dll",
+        "mpich2mpi.dll",
+        "mpich2nemesis.dll",
+        "TRANSOL.dll",
+    )
+
+    for dependency in expected_dependencies:
+        assert dependency in dep_dir_content
+
+
+def test_metaswap_dll_dep_dir_regression_contains_dependencies(
+    metaswap_dll_dep_dir_regression: Path,
+) -> None:
+    dep_dir_content = os.listdir(metaswap_dll_dep_dir_regression)
     expected_dependencies = (
         "fmpich2.dll",
         "mpich2mpi.dll",
