909be289e192e4771de00fa016e78b7e6aaee6a7
Joeri van Engelen <joerivanengelen@hotmail.com>
2022-08-16 14:44:27 +0200

    Add storage coefficient tests (#72)
    * prepare msw_model in fixture,
    parametrize sprinkling
    
    * remove idstyle
    
    * Add testcase with StorageCoefficient
    
    * Add test to compare storage coefficient
    and specific storage
    
    * lint
    
    * * head comparison now between ss and sc
    * remove comment
    * seperate fixture for storage coefficient mf6
    * storage comparison cases
    
    * isort
    
    * Also remove run_model() in storage options test
    
    * provide proper type hint
    
    * paths returned from function to tackle smelly code
    sniff
    
    * Deal with review comments

--- tests/test_metamod.py
+++ tests/test_metamod.py
@@ -1,54 +1,168 @@
 import os
 import subprocess
 from pathlib import Path
+from typing import Tuple
 
 import pytest
 from imod.couplers.metamod import MetaMod
-from imod.mf6 import Modflow6Simulation, open_cbc, open_hds
+from imod.mf6 import Modflow6Simulation, StorageCoefficient, open_cbc, open_hds
 from imod.msw import MetaSwapModel
 from numpy.testing import assert_array_almost_equal
-from pytest_cases import parametrize_with_cases
+from pytest_cases import fixture, parametrize, parametrize_with_cases
 
 
-def case_metamod_model_sprinkling(
-    coupled_mf6_model: Modflow6Simulation,
+@fixture(scope="function")
+def prepared_msw_model(
     msw_model: MetaSwapModel,
     metaswap_lookup_table: Path,
-) -> MetaMod:
-
+) -> MetaSwapModel:
     # Override unsat_svat_path with path from environment
     msw_model.simulation_settings[
         "unsa_svat_path"
     ] = msw_model._render_unsaturated_database_path(metaswap_lookup_table)
 
+    return msw_model
+
+
+@fixture(scope="function")
+def coupled_mf6_model_storage_coefficient(
+    coupled_mf6_model: Modflow6Simulation,
+) -> Modflow6Simulation:
+
+    gwf_model = coupled_mf6_model["GWF_1"]
+
+    # Specific storage package
+    sto_ds = gwf_model.pop("sto").dataset
+
+    # Confined: S = Ss * b
+    # Where 'S' is storage coefficient, 'Ss' specific
+    # storage, and 'b' thickness.
+    # https://en.wikipedia.org/wiki/Specific_storage
+
+    dis_ds = gwf_model["dis"].dataset
+    top = dis_ds["bottom"].shift(layer=1)
+    top[0] = dis_ds["top"]
+    b = top - dis_ds["bottom"]
+
+    sto_ds["storage_coefficient"] = sto_ds["specific_storage"] * b
+    sto_ds = sto_ds.drop_vars("specific_storage")
+
+    gwf_model["sto"] = StorageCoefficient(**sto_ds)
+    # reassign gwf model
+    coupled_mf6_model["GWF_1"] = gwf_model
+
+    return coupled_mf6_model
+
+
+def case_sprinkling(
+    coupled_mf6_model: Modflow6Simulation,
+    prepared_msw_model: MetaSwapModel,
+) -> MetaMod:
+
     return MetaMod(
-        msw_model,
+        prepared_msw_model,
         coupled_mf6_model,
         mf6_rch_pkgkey="rch_msw",
         mf6_wel_pkgkey="wells_msw",
     )
 
 
-def case_metamod_model_no_sprinkling(
+def case_no_sprinkling(
     coupled_mf6_model: Modflow6Simulation,
-    msw_model: MetaSwapModel,
-    metaswap_lookup_table: Path,
+    prepared_msw_model: MetaSwapModel,
 ) -> MetaMod:
 
-    # Override unsat_svat_path with path from environment
-    msw_model.simulation_settings[
-        "unsa_svat_path"
-    ] = msw_model._render_unsaturated_database_path(metaswap_lookup_table)
-
-    # Remove sprinkling package
-    msw_model.pop("sprinkling")
+    prepared_msw_model.pop("sprinkling")
 
     return MetaMod(
-        msw_model,
+        prepared_msw_model,
         coupled_mf6_model,
         mf6_rch_pkgkey="rch_msw",
-        mf6_wel_pkgkey=None,  # Do not couple to Modflow wel
+        mf6_wel_pkgkey=None,
     )
+
+
+def case_storage_coefficient(
+    coupled_mf6_model_storage_coefficient: Modflow6Simulation,
+    prepared_msw_model: MetaSwapModel,
+) -> MetaMod:
+
+    return MetaMod(
+        prepared_msw_model,
+        coupled_mf6_model_storage_coefficient,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
+
+
+def case_storage_coefficient_no_sprinkling(
+    coupled_mf6_model_storage_coefficient: Modflow6Simulation,
+    prepared_msw_model: MetaSwapModel,
+) -> MetaMod:
+
+    prepared_msw_model.pop("sprinkling")
+
+    return MetaMod(
+        prepared_msw_model,
+        coupled_mf6_model_storage_coefficient,
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey=None,
+    )
+
+
+def cases_no_sprinkling(
+    prepared_msw_model: MetaSwapModel,
+    coupled_mf6_model: Modflow6Simulation,
+    coupled_mf6_model_storage_coefficient: Modflow6Simulation,
+) -> Tuple[MetaMod]:
+    """
+    Two MetaMod objects, both without sprinkling. One with specific storage, one
+    with storage coefficient.
+    """
+
+    prepared_msw_model.pop("sprinkling")
+    kwargs = dict(
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey=None,
+    )
+
+    metamod_ss = MetaMod(prepared_msw_model, coupled_mf6_model, **kwargs)
+
+    metamod_sc = MetaMod(
+        prepared_msw_model, coupled_mf6_model_storage_coefficient, **kwargs
+    )
+
+    return metamod_ss, metamod_sc
+
+
+def cases_sprinkling(
+    prepared_msw_model: MetaSwapModel,
+    coupled_mf6_model: Modflow6Simulation,
+    coupled_mf6_model_storage_coefficient: Modflow6Simulation,
+) -> Tuple[MetaMod]:
+    """
+    Two MetaMod objects, both with sprinkling. One with specific storage, one
+    with storage coefficient.
+    """
+
+    kwargs = dict(
+        mf6_rch_pkgkey="rch_msw",
+        mf6_wel_pkgkey="wells_msw",
+    )
+
+    metamod_ss = MetaMod(
+        prepared_msw_model,
+        coupled_mf6_model,
+        **kwargs,
+    )
+
+    metamod_sc = MetaMod(
+        prepared_msw_model,
+        coupled_mf6_model_storage_coefficient,
+        **kwargs,
+    )
+
+    return metamod_ss, metamod_sc
 
 
 @pytest.fixture
@@ -63,6 +177,13 @@ def tmp_path_reg(
     tmp_path: Path,
 ) -> Path:
     return tmp_path / "regression"
+
+
+def mf6_output_files(path: Path) -> Tuple[Path]:
+    """return paths to Modflow 6 output files"""
+    path_mf6 = path / "Modflow6" / "GWF_1"
+
+    return path_mf6 / "GWF_1.hds", path_mf6 / "GWF_1.cbc", path_mf6 / "dis.dis.grb"
 
 
 def test_lookup_table_present(metaswap_lookup_table: Path) -> None:
@@ -123,7 +244,7 @@ def test_modflow_dll_present(modflow_dll_devel: Path) -> None:
     assert modflow_dll_devel.is_file()
 
 
-@parametrize_with_cases("metamod_model", cases=".")
+@parametrize_with_cases("metamod_model", cases=".", prefix="case_")
 def test_metamod_develop(
     tmp_path_dev: Path,
     metamod_model: MetaMod,
@@ -132,6 +253,9 @@ def test_metamod_develop(
     modflow_dll_devel: Path,
     imod_coupler_exec_devel: Path,
 ):
+    """
+    Test if coupled models run with the iMOD Coupler development version.
+    """
     metamod_model.write(
         tmp_path_dev,
         modflow6_dll=modflow_dll_devel,
@@ -147,8 +271,7 @@ def test_metamod_develop(
     assert len(list((tmp_path_dev / "MetaSWAP").glob("*/*.idf"))) == 24
 
     # Test if Modflow6 output written
-    headfile = tmp_path_dev / "Modflow6" / "GWF_1" / "GWF_1.hds"
-    cbcfile = tmp_path_dev / "Modflow6" / "GWF_1" / "GWF_1.cbc"
+    headfile, cbcfile, _ = mf6_output_files(tmp_path_dev)
 
     assert headfile.exists()
     assert cbcfile.exists()
@@ -158,8 +281,8 @@ def test_metamod_develop(
     assert cbcfile.stat().st_size > 0
 
 
-@parametrize_with_cases("metamod_model", cases=".")
-def test_metamod_regression_sprinkling(
+@parametrize_with_cases("metamod_model", cases=".", prefix="case_")
+def test_metamod_regression(
     metamod_model: MetaMod,
     tmp_path_dev: Path,
     tmp_path_reg: Path,
@@ -172,6 +295,11 @@ def test_metamod_regression_sprinkling(
     imod_coupler_exec_devel: Path,
     imod_coupler_exec_regression: Path,
 ):
+    """
+    Regression test if coupled models run with the iMOD Coupler development and
+    regression version. Test if results are near equal.
+    """
+
     metamod_model.write(
         tmp_path_dev,
         modflow6_dll=modflow_dll_devel,
@@ -184,9 +312,7 @@ def test_metamod_regression_sprinkling(
     )
 
     # Read Modflow 6 output
-    headfile_dev = tmp_path_dev / "Modflow6" / "GWF_1" / "GWF_1.hds"
-    cbcfile_dev = tmp_path_dev / "Modflow6" / "GWF_1" / "GWF_1.cbc"
-    grbfile_dev = tmp_path_dev / "Modflow6" / "GWF_1" / "dis.dis.grb"
+    headfile_dev, cbcfile_dev, grbfile_dev = mf6_output_files(tmp_path_dev)
 
     heads_dev = open_hds(headfile_dev, grbfile_dev)
     budgets_dev = open_cbc(cbcfile_dev, grbfile_dev)
@@ -205,9 +331,7 @@ def test_metamod_regression_sprinkling(
     )
 
     # Read Modflow 6 output
-    headfile_reg = tmp_path_reg / "Modflow6" / "GWF_1" / "GWF_1.hds"
-    cbcfile_reg = tmp_path_reg / "Modflow6" / "GWF_1" / "GWF_1.cbc"
-    grbfile_reg = tmp_path_reg / "Modflow6" / "GWF_1" / "dis.dis.grb"
+    headfile_reg, cbcfile_reg, grbfile_reg = mf6_output_files(tmp_path_reg)
 
     heads_reg = open_hds(headfile_reg, grbfile_reg)
     budgets_reg = open_cbc(cbcfile_reg, grbfile_reg)
@@ -219,4 +343,67 @@ def test_metamod_regression_sprinkling(
     for varname in budgets_dev.keys():
         assert_array_almost_equal(
             budgets_dev[varname].compute(), budgets_reg[varname].compute(), decimal=8
+        )
+
+
+@parametrize_with_cases("metamods", cases=".", prefix="cases_")
+def test_metamodel_storage_options(
+    tmp_path: Path,
+    metamods: Tuple[MetaMod],
+    metaswap_dll_devel: Path,
+    metaswap_dll_dep_dir_devel: Path,
+    modflow_dll_devel: Path,
+    imod_coupler_exec_devel: Path,
+):
+    """
+    Test and compare two coupled models with the Modflow 6 storage specified as
+    storage coefficient and as specific storage. Test if results are near equal.
+    """
+
+    metamod_ss, metamod_sc = metamods
+
+    tmp_path_sc = tmp_path / "storage_coefficient"
+    tmp_path_ss = tmp_path / "specific_storage"
+
+    metamod_ss.write(
+        tmp_path_ss,
+        modflow6_dll=modflow_dll_devel,
+        metaswap_dll=metaswap_dll_devel,
+        metaswap_dll_dependency=metaswap_dll_dep_dir_devel,
+    )
+
+    subprocess.run(
+        [imod_coupler_exec_devel, tmp_path_ss / metamod_ss._toml_name], check=True
+    )
+
+    # Read Modflow 6 output
+    headfile_ss, cbcfile_ss, grbfile_ss = mf6_output_files(tmp_path_ss)
+
+    heads_ss = open_hds(headfile_ss, grbfile_ss)
+    budgets_ss = open_cbc(cbcfile_ss, grbfile_ss)
+
+    metamod_sc.write(
+        tmp_path_sc,
+        modflow6_dll=modflow_dll_devel,
+        metaswap_dll=metaswap_dll_devel,
+        metaswap_dll_dependency=metaswap_dll_dep_dir_devel,
+    )
+
+    subprocess.run(
+        [imod_coupler_exec_devel, tmp_path_sc / metamod_sc._toml_name], check=True
+    )
+
+    # Read Modflow 6 output
+    headfile_sc, cbcfile_sc, grbfile_sc = mf6_output_files(tmp_path_sc)
+
+    heads_sc = open_hds(headfile_sc, grbfile_sc)
+    budgets_sc = open_cbc(cbcfile_sc, grbfile_sc)
+
+    assert_array_almost_equal(heads_sc.compute(), heads_ss.compute(), decimal=8)
+
+    assert budgets_sc.keys() == budgets_ss.keys()
+
+    for varname in budgets_sc.keys():
+        assert_array_almost_equal(
+            budgets_sc[varname].compute(), budgets_ss[varname].compute(), decimal=8
         )
