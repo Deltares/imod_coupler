c05456269f7f8c97899939184cb90397e0eb4cd9
Hofer-Julian <julianhofer@gnome.org>
2022-08-16 11:15:12 +0200

    Check output in test

--- tests/test_general.py
+++ tests/test_general.py
@@ -6,7 +6,7 @@ from imod_coupler import __version__
 
 def test_get_version(imod_coupler_exec_devel: Path) -> None:
     output_version = subprocess.run(
-        [imod_coupler_exec_devel, "--version"], capture_output=True, text=True
+        [imod_coupler_exec_devel, "--version"], capture_output=True, text=True, check=True
     ).stdout.strip("\n")
 
     assert output_version == __version__
