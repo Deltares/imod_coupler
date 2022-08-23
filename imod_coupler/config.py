28bf5e7b5399218449ac0073bb63edcdd6e0855e
Hofer-Julian <julianhofer@gnome.org>
2022-08-23 09:49:15 +0200

    Specify relative dir at object level

--- imod_coupler/config.py
+++ imod_coupler/config.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 from pathlib import Path
 from typing import Any
@@ -23,6 +24,11 @@ class BaseConfig(BaseModel):
     timing: bool = False
     driver_type: DriverType
     driver: BaseModel
+
+    def __init__(self, config_dir: Path, **data: Any) -> None:
+        # Validation expects current working directory at config file level
+        os.chdir(config_dir)
+        super().__init__(**data)
 
     @validator("log_file")
     def resolve_log_file(cls, log_file: Path) -> Path:
