74fe9415b2a51233a792879977057d53a9c7e9c1
Luitjan.slooten <Luitjan.slooten@deltares.nl>
2022-11-04 11:51:31 +0100

    cleanup

--- imod_coupler/drivers/dfm_metamod/mf6_utilities.py
+++ imod_coupler/drivers/dfm_metamod/mf6_utilities.py
@@ -7,30 +7,6 @@ from xmipy import XmiWrapper
 
 class MF6Utilities:
     @classmethod
-    def get_flow_model_keys(cls, mf6_simulation: Modflow6Simulation) -> list[str]:
-        """
-        returns a list of the groundwater models contained in a modflow simulation, and their keys
-        """
-        return [
-            key
-            for key, model in mf6_simulation.items()
-            if isinstance(model, GroundwaterFlowModel)
-        ]
-
-    @classmethod
-    def get_modflow_package_keys(
-        cls, mf6_flow_model: GroundwaterFlowModel, package_type: type
-    ) -> list[str]:
-        """
-        returns a list keys of packages of a particular type in a groundwater model
-        """
-        return [
-            pkgname
-            for pkgname, pkg in mf6_flow_model.items()
-            if isinstance(pkg, package_type)
-        ]
-
-    @classmethod
     def set_river_stages(
         cls,
         mf6_wrapper: XmiWrapper,
--- tests/test_mf6_utilities.py
+++ tests/test_mf6_utilities.py
@@ -9,24 +9,6 @@ from xmipy import XmiWrapper
 from imod_coupler.drivers.dfm_metamod.mf6_utilities import MF6Utilities
 
 
-def test_mf6_get_flowmodel_key(mf6_model_with_river: mf6.Modflow6Simulation) -> None:
-
-    flowmodels = MF6Utilities.get_flow_model_keys(mf6_model_with_river)
-    assert len(flowmodels) == 1
-    assert flowmodels[0] == "GWF_1"
-    assert isinstance(mf6_model_with_river[flowmodels[0]], mf6.GroundwaterFlowModel)
-
-
-def test_mf6_get_package_keys(mf6_model_with_river: mf6.Modflow6Simulation) -> None:
-
-    flowmodel_key = MF6Utilities.get_flow_model_keys(mf6_model_with_river)[0]
-    river_packs = MF6Utilities.get_modflow_package_keys(
-        mf6_model_with_river[flowmodel_key], mf6.River
-    )
-    assert len(river_packs) == 1
-    assert river_packs[0] == "Oosterschelde"
-
-
 def test_mf6_set_river_stage(
     mf6_model_with_river: mf6.Modflow6Simulation, modflow_dll_regression: Path
 ) -> None:
@@ -41,12 +23,16 @@ def test_mf6_set_river_stage(
     mf6wrapper.initialize()
     mf6wrapper.prepare_time_step(0.0)
 
-    new_river_stage = range(15)
+    new_river_stage = NDArray[np.float_](15)
+    new_river_stage[:] = range(15)
     MF6Utilities.set_river_stages(
-        mf6wrapper, mf6_model_with_river, NDArray[np.float_](new_river_stage)
+        mf6wrapper,
+        "GWF_1",
+        "Oosterschelde",
+        new_river_stage,
     )
 
     bound_adress = mf6wrapper.get_var_address("BOUND", "GWF_1", "Oosterschelde")
     bound = mf6wrapper.get_value_ptr(bound_adress)
     stage = bound[:, 0]
-    assert np.isclose(NDArray[np.float_](stage), NDArray[np.float_](stage))
+    assert np.isclose(stage, new_river_stage).all()
