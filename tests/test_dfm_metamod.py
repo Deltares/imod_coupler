0abcfce0d4d07881bc5955168bfa2a883fc32516
Hofer-Julian <julianhofer@gnome.org>
2022-11-04 10:44:05 +0100

    Small fix in test_dfm_metamod.py

--- tests/test_dfm_metamod.py
+++ tests/test_dfm_metamod.py
@@ -7,10 +7,8 @@ from tempfile import tempdir
 import pytest
 from pytest_cases import parametrize_with_cases
 
-from imod_coupler import __main__
+from imod_coupler.__main__ import run_coupler
 from imod_coupler.drivers.dfm_metamod.dfm_metamod_model import DfmMetaModModel
-
-testdir = tempfile.mkdtemp()
 
 
 @parametrize_with_cases("dfm_metamod", prefix="case_default")
@@ -21,12 +19,13 @@ def test_dfmmetamod_initialization(
     metaswap_dll_dep_dir_devel: Path,
     dflowfm_dll: Path,
     dflowfm_initial_inputfiles_folder: Path,
+    tmp_path_dev: Path,
 ):
 
     # write input for the simulators and a toml configuration file containing the paths of these
     # inputfiles and the paths of the kernel dll's
     dfm_metamod.write(
-        testdir,
+        tmp_path_dev,
         modflow6_dll=modflow_dll_devel,
         metaswap_dll=metaswap_dll_devel,
         metaswap_dll_dependency=metaswap_dll_dep_dir_devel,
@@ -40,13 +39,13 @@ def test_dfmmetamod_initialization(
     # so as a temporary hack we copy these files into the DfmMetamod output directory
     inifiles_dir = dflowfm_initial_inputfiles_folder
     for f in listdir(inifiles_dir):
-        shutil.copy(inifiles_dir.joinpath(f), testdir + "/dflow-fm")
+        shutil.copy(inifiles_dir.joinpath(f), tmp_path_dev / "dflow-fm")
 
-    inputpath = Path(testdir + sep + dfm_metamod._toml_name)
+    inputpath = tmp_path_dev / dfm_metamod._toml_name
 
     # at this stage, imod_coupler is unable to complete the run with these 3 models and kernels.
     # so we check we get an exception that originates in the "couple" method - getting there
     # means the initialization of the kernels went well.
     with pytest.raises(ValueError) as e:
-        __main__.run_coupler(inputpath)
+        run_coupler(inputpath)
     assert str(e.value) == "Can't find MetaSWAP\\mod2svat.inp."
