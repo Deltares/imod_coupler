30cd639bf8b0d1bd1f55867c08bde769ac6707c7
Hofer-Julian <julianhofer@gnome.org>
2022-11-09 10:57:03 +0100

    Remove last skip marker

--- tests/test_dfm_wrapper.py
+++ tests/test_dfm_wrapper.py
@@ -37,3 +37,30 @@ def test_get_river_stage_from_dflow(
 
     # the current test dataset does not have 1d rivers.
     assert water_levels_1d is None
+
+
+# def test_get_snapped_feature(
+#     prepared_dflowfm_model: FMModel,
+#     dflowfm_dll_devel: Path,
+#     dflowfm_initial_inputfiles_folder: Path,
+#     tmp_path_dev: Path,
+# ) -> None:
+
+#     prepared_dflowfm_model.save(recurse=True)
+#     prepared_dflowfm_model.filepath = tmp_path_dev / "fm.mdu"
+
+#     set_dfm_path(dflowfm_dll_devel)
+#     copy_inputfiles(
+#         dflowfm_initial_inputfiles_folder, prepared_dflowfm_model.filepath.parent
+#     )
+
+#     bmiwrapper = DfmWrapper(
+#         engine="dflowfm", configfile=prepared_dflowfm_model.filepath
+#     )
+
+#     bmiwrapper.initialize()
+#     water_levels_1d = bmiwrapper.get_waterlevels_1d()
+#     bmiwrapper.finalize()
+
+#     # the current test dataset does not have 1d rivers.
+#     assert water_levels_1d is None
