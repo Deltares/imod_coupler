a2d4c586402038636b4cccd504e2cd5f8dd53cd0
luitjansl <64598682+luitjansl@users.noreply.github.com>
2022-11-10 10:46:50 +0100

    Update tests/test_dfm_wrapper.py
    Co-authored-by: Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>

--- tests/test_dfm_wrapper.py
+++ tests/test_dfm_wrapper.py
@@ -55,7 +55,7 @@ def test_get_river_stage(
 
 def test_get_cumulative_fluxes_1d_nodes(
     tmodel_input_folder: Path,
-    dflowfm_dll_regression: Path,
+    dflowfm_dll_devel: Path,
     tmp_path_reg: Path,
 ) -> None:
 
