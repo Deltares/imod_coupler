2c9c6206fb0a581f311a9f24ed828d5a7ed03424
Luitjan.slooten <Luitjan.slooten@deltares.nl>
2022-11-11 12:54:50 +0100

    changed test order to make tests succeed

--- tests/test_dfm_wrapper.py
+++ tests/test_dfm_wrapper.py
@@ -10,6 +10,36 @@ from numpy.testing import assert_array_equal
 from imod_coupler.drivers.dfm_metamod.dfm_wrapper import DfmWrapper
 
 
+def test_get_snapped_flownode(
+    prepared_dflowfm_model: FMModel,
+    dflowfm_dll_devel: Path,
+    dflowfm_initial_inputfiles_folder: Path,
+) -> None:
+
+    prepared_dflowfm_model.save(recurse=True)
+
+    set_dfm_path(dflowfm_dll_devel)
+    copy_inputfiles(
+        dflowfm_initial_inputfiles_folder, prepared_dflowfm_model.filepath.parent
+    )
+
+    bmiwrapper = DfmWrapper(
+        engine="dflowfm", configfile=prepared_dflowfm_model.filepath
+    )
+
+    bmiwrapper.initialize()
+    input_node_x = np.array([150.0, 150.0, 450.0])
+    input_node_y = np.array([150.0, 250.0, 250.0])
+    flownode_ids = bmiwrapper.get_snapped_flownode(input_node_x, input_node_y)
+    bmiwrapper.finalize()
+
+    excepted_flownode_ids = np.array([1, 2, 8])
+    assert_array_equal(
+        flownode_ids,
+        excepted_flownode_ids,
+    )
+
+
 def test_get_river_stage(
     tmodel_input_folder: Path,
     dflowfm_dll_devel: Path,
@@ -122,33 +152,3 @@ def test_set_1d_river_fluxes(
     bmiwrapper.finalize()
     assert new_fluxes is not None
     np.testing.assert_allclose(fluxes, new_fluxes)
-
-
-def test_get_snapped_flownode(
-    prepared_dflowfm_model: FMModel,
-    dflowfm_dll_devel: Path,
-    dflowfm_initial_inputfiles_folder: Path,
-) -> None:
-
-    prepared_dflowfm_model.save(recurse=True)
-
-    set_dfm_path(dflowfm_dll_devel)
-    copy_inputfiles(
-        dflowfm_initial_inputfiles_folder, prepared_dflowfm_model.filepath.parent
-    )
-
-    bmiwrapper = DfmWrapper(
-        engine="dflowfm", configfile=prepared_dflowfm_model.filepath
-    )
-
-    bmiwrapper.initialize()
-    input_node_x = np.array([150.0, 150.0, 450.0])
-    input_node_y = np.array([150.0, 250.0, 250.0])
-    flownode_ids = bmiwrapper.get_snapped_flownode(input_node_x, input_node_y)
-    bmiwrapper.finalize()
-
-    excepted_flownode_ids = np.array([1, 2, 8])
-    assert_array_equal(
-        flownode_ids,
-        excepted_flownode_ids,
-    )
