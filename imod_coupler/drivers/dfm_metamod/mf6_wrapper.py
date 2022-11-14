d29aed7d3ad85263414eb0d0053009f838dee5b2
HendrikKok <59684726+HendrikKok@users.noreply.github.com>
2022-11-14 09:01:20 +0100

    IMOD-946: add (#107)
    * IMOD-946: add
    function to add correction to the mf6 wel package
    
    * changes after review

--- imod_coupler/drivers/dfm_metamod/mf6_wrapper.py
+++ imod_coupler/drivers/dfm_metamod/mf6_wrapper.py
@@ -38,3 +38,34 @@ class Mf6Wrapper(XmiWrapper):
         if new_river_stages is None or len(new_river_stages) != len(stage):
             raise ValueError(f"Expected size of new_river_stages is {len(stage)}")
         stage[:] = new_river_stages[:]
+
+    def set_correction_flux(
+        self,
+        mf6_flowmodel_key: str,
+        mf6_wel_pkg_key: str,
+        correction_flux: Optional[NDArray[np.float_]],
+    ) -> None:
+        """
+        sets the river correction flux in a modflow simulation via the well package
+
+        Parameters
+        ----------
+        mf6_flowmodel_key : str
+            key of the modflow model
+        mf6_wel_pkg_key : str
+            key of the wel package used for the correction flux
+        correction_flux : NDArray[np.float_]
+            correction flux to be set to modflow
+
+
+        Raises
+        ------
+        ValueError
+            the size of the provided flux array does not match the expected size
+        """
+        bound_adress = self.get_var_address("BOUND", mf6_flowmodel_key, mf6_wel_pkg_key)
+        flux = self.get_value_ptr(bound_adress)
+        if correction_flux is None or len(correction_flux) != len(flux):
+            raise ValueError(f"Expected size of correction_flux is {len(flux)}")
+        flux[:] = correction_flux[:]
+        self.set_value(bound_adress, flux)
