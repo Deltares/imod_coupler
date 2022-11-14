b38a943d8e4f507a030cd75d5f02497217733702
Luitjan.slooten <Luitjan.slooten@deltares.nl>
2022-11-14 12:10:10 +0100

    added template file para_sim_template.inp

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -6,6 +6,9 @@ def fill_para_sim_template(msw_folder: Path, path_unsat_dbase: Path) -> None:
     Fill para_sim.inp template in the folder with the path to the unsaturated
     zone database.
     """
+    template_file = msw_folder / "para_sim_template.inp"
+    if not template_file.exists():
+        raise ValueError(f"could not find file {template_file}")
     with open(msw_folder / "para_sim_template.inp") as f:
         para_sim_text = f.read()
 
