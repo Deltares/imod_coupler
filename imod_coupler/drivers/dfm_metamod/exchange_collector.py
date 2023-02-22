0aaf1669b91c1a629b811479edcc91290856b4c5
rleander <Robert.Leander@deltares.nl>
2023-02-22 17:18:53 +0100

    Issue: IMOD6-996 Filled the exchange collector for the NetCDF4 case and extended the corresponding test_exchange_collector

--- imod_coupler/drivers/dfm_metamod/exchange_collector.py
+++ imod_coupler/drivers/dfm_metamod/exchange_collector.py
@@ -1,8 +1,10 @@
 
 import abc
+import os
 from pathlib import Path
 from typing import Any
 
+import netCDF4 as nc
 import numpy as np
 from numpy.typing import NDArray
 
@@ -11,19 +13,40 @@ class AbstractExchange(abc.ABC):
     def __init__(self, name:str):
         pass
 
-    def write_exchange(self, exchange: NDArray[Any], time: float, iteration: int =0)->None:
+    def write_exchange(self, exchange: NDArray[Any], time: float, iteration: int = 0) -> None:
         pass
+
 
 class NetcdfExchangeLogger(AbstractExchange):
     output_file: Path
     
-    def __init__(self,  name:str, output_dir: Path, properties: dict[str, Any]):
-        output_file = Path.joinpath(output_dir, name)
+    def __init__(
+            self, name: str, output_dir: Path, properties: dict[str, Any]):
+        if not(os.path.isdir(output_dir)):
+            os.mkdir(output_dir)
+        output_file = Path.joinpath(output_dir, name + '.nc')
+        self.ds = nc.Dataset(output_file, "w")
 
+    def initfile(self, ndx: int) -> None:
+        self.nodedim = self.ds.createDimension('id', ndx)
+        self.timedim = self.ds.createDimension('time', )
+        self.timevar = self.ds.createVariable("time", "f8", ("time", ))
+        self.datavar = self.ds.createVariable('xchg', "f8", ("time", "id", ))        
+        self.pos = 0
 
-    def write_exchange(self, exchange: NDArray[Any], time: float, iteration: int =0)->None:
-        pass    
+    def write_exchange(
+            self, exchange: NDArray[Any], time: float,
+                iteration: int = 0, sync: bool = False) -> None:
+        if len(self.ds.dimensions) == 0:
+            self.initfile(len(exchange))
+        self.timevar[self.pos] = time
+        self.datavar[self.pos, :] = exchange[:]
+        self.pos += 1
+        if sync:
+            self.ds.sync()
 
+    def finalize(self) -> None:
+        self.ds.close()
 
 
 class ExchangeCollector:
@@ -42,7 +65,7 @@ class ExchangeCollector:
             self.exchanges[exchange_name] = self.create_exchange_object(exchange_name, dict_def[0])
 
     def log_exchange(self, name: str, exchange: NDArray[Any] , time: float, iteration: int =0) -> None:
-        if not name in self.exchanges.keys():
+        if name not in self.exchanges.keys():
             raise ValueError(" unkwnown exchange logger: " + name)
         self.exchanges[name].write_exchange(exchange, time, iteration)
 
@@ -53,5 +76,9 @@ class ExchangeCollector:
                 return NetcdfExchangeLogger(flux_name, self.output_dir, dict_def)
             case _:
                 raise ValueError("unkwnown type of exchange logger")
+
+    def finalize(self) -> None:
+        for exchange in self.exchanges.values():
+            exchange.finalize()
         
         
--- tests/test_exchange_collector.py
+++ tests/test_exchange_collector.py
@@ -1,3 +1,7 @@
+import os
+from pathlib import Path
+
+import netCDF4 as nc
 import numpy as np
 import tomli
 from numpy.typing import NDArray
@@ -5,7 +9,7 @@ from numpy.typing import NDArray
 from imod_coupler.drivers.dfm_metamod.exchange_collector import ExchangeCollector
 
 
-def test_exchange_collector_read() -> None:
+def test_exchange_collector_read(tmp_path_dev: Path) -> None:
     input_file_content = """
                         [[general]]
                         output_dir = "."
@@ -14,15 +18,31 @@ def test_exchange_collector_read() -> None:
                         
                         [[exchanges.mf-ridv2dflow1d_flux_output]]
                         type = "netcdf"
+                        postfix = "in"
 
                         [[exchanges.dflow1d2mf-riv_stage_output]]
                         type = "netcdf"
+                        postfix = "out"
                         """
-
     config_dict = tomli.loads(input_file_content)
     exchange_collector = ExchangeCollector(config_dict)
 
-    some_array: NDArray[np.float_] = NDArray[np.float_](
+    some_array0: NDArray[np.float_] = NDArray[np.float_](
         (5,), buffer=np.array([1.1, 2.0, -4.8, np.nan, 3])
     )
-    exchange_collector.log_exchange("mf-ridv2dflow1d_flux_output", some_array, 8.0)
+    some_array1: NDArray[np.float_] = NDArray[np.float_](
+        (5,), buffer=np.array([1.1, 666.0, -4.8, 0.0, 3])
+    )
+    exchange_collector.log_exchange("mf-ridv2dflow1d_flux_output", some_array0, 8.0)
+    exchange_collector.log_exchange("mf-ridv2dflow1d_flux_output", some_array1, 23.0)
+    exchange_collector.finalize()
+
+    ds = nc.Dataset("./mf-ridv2dflow1d_flux_output.nc", "r")
+    dat = ds.variables['xchg'][:]
+    tim = ds.variables['time'][:]
+    assert np.array_equal(dat[0, :], some_array0, equal_nan=True)
+    assert np.array_equal(dat[1, :], some_array1, equal_nan=True)
+    assert np.array_equal(tim[:], np.array([8.0, 23.0]), equal_nan=True)
+
+
+
