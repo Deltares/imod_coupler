c4a5ed68e2036c266e1238ff22586b93131b298d
rleander <Robert.Leander@deltares.nl>
2023-02-23 13:39:02 +0100

    Now runs in pytest temp dir

--- tests/test_exchange_collector.py
+++ tests/test_exchange_collector.py
@@ -11,26 +11,36 @@ from imod_coupler.drivers.dfm_metamod.exchange_collector import ExchangeCollecto
 
 
 def test_exchange_collector_read(tmp_path_dev: Path, output_config_toml: str) -> None:
-    config_dict = tomli.loads(output_config_toml)
-    exchange_collector = ExchangeCollector(config_dict)
+    cwd0 = os.getcwd()
+    if not (os.path.isdir(tmp_path_dev)):
+        os.makedirs(tmp_path_dev)
+    os.chdir(tmp_path_dev)
 
-    some_array0: NDArray[np.float_] = NDArray[np.float_](
-        (5,), buffer=np.array([1.1, 2.0, -4.8, np.nan, 3])
-    )
-    some_array1: NDArray[np.float_] = NDArray[np.float_](
-        (5,), buffer=np.array([1.1, 666.0, -4.8, 0.0, 3])
-    )
-    exchange_collector.log_exchange("mf-ridv2dflow1d_flux_output", some_array0, 8.0)
-    exchange_collector.log_exchange("mf-ridv2dflow1d_flux_output", some_array1, 23.0)
-    exchange_collector.finalize()
-
-    ds = nc.Dataset("./mf-ridv2dflow1d_flux_output.nc", "r")
-    dat = ds.variables["xchg"][:]
-    tim = ds.variables["time"][:]
-    assert np.array_equal(dat[0, :], some_array0, equal_nan=True)
-    assert np.array_equal(dat[1, :], some_array1, equal_nan=True)
-    assert np.array_equal(tim[:], np.array([8.0, 23.0]), equal_nan=True)
-
+    try:
+        config_dict = tomli.loads(output_config_toml)
+        exchange_collector = ExchangeCollector(config_dict)
+    
+        some_array0: NDArray[np.float_] = NDArray[np.float_](
+            (5,), buffer=np.array([1.1, 2.0, -4.8, np.nan, 3])
+        )
+        some_array1: NDArray[np.float_] = NDArray[np.float_](
+            (5,), buffer=np.array([1.1, 666.0, -4.8, 0.0, 3])
+        )
+        exchange_collector.log_exchange(
+            "mf-ridv2dflow1d_flux_output", some_array0, 8.0)
+        exchange_collector.log_exchange(
+            "mf-ridv2dflow1d_flux_output", some_array1, 23.0)
+        exchange_collector.finalize()
+    
+        ds = nc.Dataset("./mf-ridv2dflow1d_flux_output.nc", "r")
+        dat = ds.variables["xchg"][:]
+        tim = ds.variables["time"][:]
+        assert np.array_equal(dat[0, :], some_array0, equal_nan=True)
+        assert np.array_equal(dat[1, :], some_array1, equal_nan=True)
+        assert np.array_equal(tim[:], np.array([8.0, 23.0]), equal_nan=True)
+    finally:
+        os.chdir(cwd0)
+    
 
 def test_exchange_collector_ignores_unknown_exchanges(
     tmp_path_dev: Path, output_config_toml: str
