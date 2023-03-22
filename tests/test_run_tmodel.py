32c0253aec7e7430359a75cc02c9828fe69e4f1d
Hendrik Kok <hendrik.kok@deltares.nl>
2023-03-22 11:30:21 +0100

    refactor test_run_tmodel + add tmodel f

--- tests/test_run_tmodel.py
+++ tests/test_run_tmodel.py
@@ -9,18 +9,13 @@ from test_utilities import fill_para_sim_template, numeric_csvfiles_equal
 from imod_coupler.__main__ import run_coupler
 
 
-def test_run_tmodel(
+def fill_toml_tmodel(
     tmp_path_dev: Path,
-    tmodel_input_folder: Path,
     modflow_dll_devel: Path,
     dflowfm_dll: Path,
     metaswap_dll_devel: Path,
     metaswap_dll_dep_dir_devel: Path,
-    metaswap_lookup_table: Path,
-    reference_result_folder: Path,
-) -> None:
-    shutil.copytree(tmodel_input_folder, tmp_path_dev)
-
+) -> Path:
     toml_file_path = tmp_path_dev / "imod_coupler.toml"
     toml_dict = {}
     with open(toml_file_path, "rb") as f:
@@ -34,21 +29,16 @@ def test_run_tmodel(
     )
     with open(toml_file_path, "wb") as toml_file:
         tomli_w.dump(toml_dict, toml_file)
+    return toml_file_path
 
-    fill_para_sim_template(tmp_path_dev / "MetaSWAP", metaswap_lookup_table)
-
-    run_coupler(toml_file_path)
-
+def evaluate_waterbalance_tmodel(tmp_path_dev:Path,reference_result_folder:Path,name:str) -> None:
     waterbalance_result = run_waterbalance_script_on_tmodel(tmp_path_dev)
-
     csv_reference_file = (
-        reference_result_folder / "test_run_tmodel" / "waterbalance.csv"
+        reference_result_folder / name / "waterbalance.csv"
     )
-
     assert numeric_csvfiles_equal(
         waterbalance_result, csv_reference_file, ";", abstol=5600.0, reltol=3.5
     )
-
 
 def run_waterbalance_script_on_tmodel(testdir: Path) -> Path:
     modflow_out_file = testdir / "Modflow6" / "GWF_1" / "T-MODEL-D.LST"
@@ -60,3 +50,38 @@ def run_waterbalance_script_on_tmodel(testdir: Path) -> Path:
         dflow_out_file, metaswap_out_file, modflow_out_file, output_file_csv=csv_file
     )
     return csv_file
+
+def test_run_tmodel(
+    tmp_path_dev: Path,
+    tmodel_input_folder: Path,
+    modflow_dll_devel: Path,
+    dflowfm_dll: Path,
+    metaswap_dll_devel: Path,
+    metaswap_dll_dep_dir_devel: Path,
+    metaswap_lookup_table: Path,
+    reference_result_folder: Path,
+) -> None:
+    shutil.copytree(tmodel_input_folder, tmp_path_dev)
+    
+    toml_file_path = fill_toml_tmodel(tmp_path_dev,modflow_dll_devel,dflowfm_dll,metaswap_dll_devel,metaswap_dll_dep_dir_devel)
+    fill_para_sim_template(tmp_path_dev / "MetaSWAP", metaswap_lookup_table)
+    run_coupler(toml_file_path)
+    # evaluate_waterbalance_tmodel(tmp_path_dev,reference_result_folder,'test_run_tmodel')
+
+def test_run_tmodel_f(
+    tmp_path_dev: Path,
+    tmodel_f_input_folder: Path,
+    modflow_dll_devel: Path,
+    dflowfm_dll: Path,
+    metaswap_dll_devel: Path,
+    metaswap_dll_dep_dir_devel: Path,
+    metaswap_lookup_table: Path,
+    reference_result_folder: Path,
+) -> None:
+    shutil.copytree(tmodel_f_input_folder, tmp_path_dev)
+    
+    toml_file_path = fill_toml_tmodel(tmp_path_dev,modflow_dll_devel,dflowfm_dll,metaswap_dll_devel,metaswap_dll_dep_dir_devel)
+    fill_para_sim_template(tmp_path_dev / "MetaSWAP", metaswap_lookup_table)
+    run_coupler(toml_file_path)
+    # evaluate_waterbalance_tmodel(tmp_path_dev,reference_result_folder,'test_run_tmodel')
+
