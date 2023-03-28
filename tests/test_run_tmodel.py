252f65f037af38ed72d21e5caefecaf5275231cd
luitjan <luitjan.slooten@deltares.nl>
2023-03-28 12:25:03 +0200

    setting up test

--- tests/test_run_tmodel.py
+++ tests/test_run_tmodel.py
@@ -172,3 +172,51 @@ def test_run_tmodel_f_without_dflow(
         [str(imod_coupler_exec_devel), toml_file_path],
         check=True,
     )
+
+
+def test_run_tmodel_f_without_dflow_with_sprinkling(
+    tmp_path_dev: Path,
+    tmodel_f_input_folder: Path,
+    modflow_dll_devel: Path,
+    dflowfm_dll: Path,
+    metaswap_dll_devel: Path,
+    metaswap_dll_dep_dir_devel: Path,
+    metaswap_lookup_table: Path,
+    reference_result_folder: Path,
+    imod_coupler_exec_devel: Path,
+) -> None:
+    """
+    This test aims to remove all the exchanges that involve dflow, so that we can compare the result of
+    the simulation with the metamod driver's result for the same setup.
+    """
+    shutil.copytree(tmodel_f_input_folder, tmp_path_dev)
+    toml_file_path = tmp_path_dev / "imod_coupler.toml"
+    output_config_path = tmp_path_dev / "output_config.toml"
+
+    set_toml_file_tmodel(
+        toml_file_path,
+        modflow_dll_devel,
+        metaswap_dll_devel,
+        metaswap_dll_dep_dir_devel,
+        dflowfm_dll,
+    )
+    files_to_skip = {
+        "msw_ponding_to_dfm_2d_dv_dmm",
+        "dfm_2d_waterlevels_to_msw_h_dmm",
+        "mf6_river2_to_dmf_1d_q_dmm",
+        "mf6_drainage_to_dfm_1d_q_dmm",
+        "msw_sprinkling_to_dfm_1d_q_dmm",
+        "msw_runoff_to_dfm_1d_q_dmm",
+        "mf6_river_to_dfm_1d_q_dmm",
+        "dfm_1d_waterlevel_to_mf6_river_stage_dmm",
+    }
+
+    remove_exchange_file_references(toml_file_path, files_to_skip)
+
+    set_workdir_in_logging_config_file(output_config_path, tmp_path_dev)
+    fill_para_sim_template(tmp_path_dev / "MetaSWAP", metaswap_lookup_table)
+
+    subprocess.run(
+        [str(imod_coupler_exec_devel), toml_file_path],
+        check=True,
+    )
