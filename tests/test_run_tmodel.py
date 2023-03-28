a36ca89c44705ddb5863af5b210aa6935f4494ac
luitjan <luitjan.slooten@deltares.nl>
2023-03-28 14:35:57 +0200

    adding input for sprinkling

--- tests/test_run_tmodel.py
+++ tests/test_run_tmodel.py
@@ -84,6 +84,8 @@ def test_run_tmodel_f_no_sprinkling(
     remove_exchange_file_references(toml_file_path, files_to_skip)
     set_workdir_in_logging_config_file(output_config_path, tmp_path_dev)
     fill_para_sim_template(tmp_path_dev / "MetaSWAP", metaswap_lookup_table)
+
+    run_coupler(toml_file_path)
     subprocess.run(
         [str(imod_coupler_exec_devel), toml_file_path],
         check=True,
@@ -216,7 +218,4 @@ def test_run_tmodel_f_without_dflow_with_sprinkling(
     set_workdir_in_logging_config_file(output_config_path, tmp_path_dev)
     fill_para_sim_template(tmp_path_dev / "MetaSWAP", metaswap_lookup_table)
 
-    subprocess.run(
-        [str(imod_coupler_exec_devel), toml_file_path],
-        check=True,
-    )
+    run_coupler(toml_file_path)
