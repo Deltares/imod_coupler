0fb8e1b9aa5c8c011a8cc512f6ab53a616700876
Hendrik Kok <hendrik.kok@deltares.nl>
2023-04-04 15:45:03 +0200

    black

--- tests/test_run_tmodel.py
+++ tests/test_run_tmodel.py
@@ -73,9 +73,9 @@ def test_run_tmodel_f(
     set_workdir_in_logging_config_file(output_config_path, tmp_path_dev)
     fill_para_sim_template(tmp_path_dev / "MetaSWAP", metaswap_lookup_table)
 
-    # run_coupler(toml_file_path)
+    run_coupler(toml_file_path)
     evaluate_waterbalance(tmp_path_dev, reference_result_folder, "T-MODEL-F.LST")
-    subprocess.run(
-        [str(imod_coupler_exec_devel), toml_file_path],
-        check=True,
-    )
+    # subprocess.run(
+    #    [str(imod_coupler_exec_devel), toml_file_path],
+    #    check=True,
+    # )
