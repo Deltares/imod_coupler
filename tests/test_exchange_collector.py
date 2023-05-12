d935147ad16f0a5d26af06da08a35c3576a7a1e7
luitjan <luitjan.slooten@deltares.nl>
2023-05-12 15:50:26 +0200

    review comments

--- tests/test_exchange_collector.py
+++ tests/test_exchange_collector.py
@@ -20,12 +20,9 @@ def test_exchange_collector_read(tmp_path_dev: Path, output_config_toml: str) ->
     config_dict["general"]["output_dir"] = tmp_path_dev
     exchange_collector = ExchangeCollector.from_config(config_dict)
 
-    some_array0: NDArray[np.float_] = NDArray[np.float_](
-        (5,), buffer=np.array([1.1, 2.0, -4.8, np.nan, 3])
-    )
-    some_array1: NDArray[np.float_] = NDArray[np.float_](
-        (5,), buffer=np.array([1.1, 666.0, -4.8, 0.0, 3])
-    )
+    some_array0: NDArray[np.float_] = np.array([1.1, 2.0, -4.8, np.nan, 3])
+    some_array1: NDArray[np.float_] = np.array([1.1, 666.0, -4.8, 0.0, 3])
+
     exchange_collector.log_exchange("example_flux_output", some_array0, 8.0)
     exchange_collector.log_exchange("example_flux_output", some_array1, 23.0)
     exchange_collector.finalize()
@@ -49,12 +46,9 @@ def test_exchange_collector_ignores_unknown_exchanges(
     config_dict["general"]["output_dir"] = tmp_path_dev
     exchange_collector = ExchangeCollector.from_config(config_dict)
 
-    some_array0: NDArray[np.float_] = NDArray[np.float_](
-        (5,), buffer=np.array([1.1, 2.0, -4.8, np.nan, 3])
-    )
-    some_array1: NDArray[np.float_] = NDArray[np.float_](
-        (5,), buffer=np.array([1.1, 666.0, -4.8, 0.0, 3])
-    )
+    some_array0: NDArray[np.float_] = np.array([1.1, 2.0, -4.8, np.nan, 3])
+    some_array1: NDArray[np.float_] = np.array([1.1, 666.0, -4.8, 0.0, 3])
+
     exchange_collector.log_exchange("non_existing_type", some_array0, 8.0)
     exchange_collector.log_exchange("non_existing_type", some_array1, 23.0)
     exchange_collector.finalize()
@@ -70,12 +64,9 @@ def test_exchange_collector_raises_exception_when_array_size_varies(
     config_dict["general"]["output_dir"] = tmp_path_dev
     exchange_collector = ExchangeCollector.from_config(config_dict)
 
-    some_array: NDArray[np.float_] = NDArray[np.float_](
-        (5,), buffer=np.array([1.1, 2.0, -4.8, np.nan, 3])
-    )
-    some_smaller_array: NDArray[np.float_] = NDArray[np.float_](
-        (4,), buffer=np.array([1.1, 666.0, -4.8, 0.0])
-    )
+    some_array: NDArray[np.float_] = np.array([1.1, 2.0, -4.8, np.nan, 3])
+    some_smaller_array: NDArray[np.float_] = np.array([1.1, 666.0, -4.8, 0.0])
+
     exchange_collector.log_exchange("example_stage_output", some_array, 8.0)
     with pytest.raises(ValueError) as e:
         exchange_collector.log_exchange(
@@ -102,10 +93,10 @@ def test_exchange_collector_overwrites_when_time_is_repeated(
     exchange_collector = ExchangeCollector.from_config(config_dict)
 
     some_arrays: list[NDArray[np.float_]] = [
-        NDArray[np.float_]((5,), buffer=np.array([1.1, 2.0, -4.8, np.nan, 1])),
-        NDArray[np.float_]((5,), buffer=np.array([1.1, 2.2, -5.8, np.nan, 2])),
-        NDArray[np.float_]((5,), buffer=np.array([1.1, 2.0, 8.8, np.nan, 3])),
-        NDArray[np.float_]((5,), buffer=np.array([1.5, 8.0, 2.8, 6.0, -3])),
+        np.array([1.1, 2.0, -4.8, np.nan, 1]),
+        np.array([1.1, 2.2, -5.8, np.nan, 2]),
+        np.array([1.1, 2.0, 8.8, np.nan, 3]),
+        np.array([1.5, 8.0, 2.8, 6.0, -3]),
     ]
 
     exchange_collector.log_exchange("example_stage_output", some_arrays[0], 8.0)
