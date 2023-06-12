7342a36e0c22df94b7decf600f83ebc3d551b98f
Hendrik Kok <hendrik.kok@deltares.nl>
2023-06-12 15:46:37 +0200

    update test to support idf evaluation instead of csv + update reference data

--- tests/test_modstrip_full_range_dbase.py
+++ tests/test_modstrip_full_range_dbase.py
@@ -1,11 +1,13 @@
 import shutil
 import subprocess
 from pathlib import Path
+from typing import Union
 
 import imod
 import numpy as np
 import pandas as pd
 import tomli_w
+import xarray as xr
 from numpy.testing import assert_allclose
 
 from imod_coupler.__main__ import run_coupler
@@ -64,11 +66,21 @@ def fill_para_sim_template(msw_folder, path_unsat_dbase):
         f.write(para_sim_text)
 
 
-def total_flux_error(q_test, q_ref):
+def total_flux_error(
+    q_test: Union[np.ndarray, xr.DataArray, xr.Dataset],
+    q_ref: Union[np.ndarray, xr.DataArray, xr.Dataset],
+) -> np.array:
     """
     Computes total relative flux error compared to a reference flux.
+    shape of input arrays = time, row, col
     """
-    return np.abs(q_test - q_ref).sum() / np.abs(q_ref).sum()
+    if isinstance(q_test, xr.DataArray):
+        q_test = q_test.values
+    if isinstance(q_ref, xr.DataArray):
+        q_ref = q_ref.values
+    return np.squeeze(
+        np.sum(np.abs(q_test - q_ref), axis=0) / np.sum(np.abs(q_ref), axis=0)
+    )
 
 
 def test_modstrip_data_present(modstrip_full_range_dbase_loc):
@@ -128,123 +140,96 @@ def test_modstrip_model(
     assert cbcfile.stat().st_size > 0
 
     # read msw output
-    area = imod.idf.open(tmp_path / "msw" / "bdgqmodf" / "area_L1.IDF")
-    qmodf = imod.idf.open(tmp_path / "msw" / "bdgqmodf" / "bdgqmodf_*_L1.IDF") * area
-    qmsw = imod.idf.open(tmp_path / "msw" / "msw_qsim" / "msw_qsim_*_L1.IDF") * area
-    # qmsw_cor = imod.idf.open(tmp_path / "msw" / "msw_qsimcorrmf" / "bdgqsimcorrmf_*_L1.IDF") * area
-    hgw = imod.idf.open(tmp_path / "msw" / "msw_Hgw" / "msw_Hgw_*_L1.IDF")
+    area = imod.idf.open(tmp_path / "msw" / "bdgqmodf" / "area_L1.IDF").isel(
+        layer=0, drop=True
+    )
+    qmodf = imod.idf.open(tmp_path / "msw" / "bdgqmodf" / "bdgqmodf_*_L1.IDF").isel(
+        layer=0, drop=True
+    )
+    qmsw = imod.idf.open(tmp_path / "msw" / "msw_qsim" / "msw_qsim_*_L1.IDF").isel(
+        layer=0, drop=True
+    )
+    # qmsw_cor = imod.idf.open(tmp_path / "msw" / "msw_qsimcorrmf" / "bdgqsimcorrmf_*_L1.IDF")
+    hgw = imod.idf.open(tmp_path / "msw" / "msw_Hgw" / "msw_Hgw_*_L1.IDF").isel(
+        layer=0, drop=True
+    )
     dhgw = hgw.diff("time")
-    hgw_mod = imod.idf.open(tmp_path / "msw" / "msw_Hgwmodf" / "msw_Hgwmodf_*_L1.IDF")
-    sc1 = imod.idf.open(tmp_path / "msw" / "msw_sc1" / "msw_sc1_*_L1.IDF") * area
+    hgw_mod = imod.idf.open(
+        tmp_path / "msw" / "msw_Hgwmodf" / "msw_Hgwmodf_*_L1.IDF"
+    ).isel(layer=0, drop=True)
+    sc1 = imod.idf.open(tmp_path / "msw" / "msw_sc1" / "msw_sc1_*_L1.IDF").isel(
+        layer=0, drop=True
+    )
 
     # reference output
-    qmodf_ref = (
-        imod.idf.open(
-            modstrip_full_range_dbase_loc / "results" / "bdgqmodf" / "bdgqmodf_*_L1.IDF"
-        )
-        * area
-    )
-    qmsw_ref = (
-        imod.idf.open(
-            modstrip_full_range_dbase_loc / "results" / "bdgqmsw" / "bdgqmsw_*_L1.IDF"
-        )
-        * area
-    )
-    hgw_ref = (
-        imod.idf.open(
-            modstrip_full_range_dbase_loc / "results" / "msw_Hgw" / "msw_Hgw_*_L1.IDF"
-        )
-        * area
-    )
+    qmodf_ref = imod.idf.open(
+        modstrip_full_range_dbase_loc / "results" / "bdgqmodf" / "bdgqmodf_*_L1.IDF"
+    ).isel(layer=0, drop=True)
+    qmsw_ref = imod.idf.open(
+        modstrip_full_range_dbase_loc / "results" / "msw_qsim" / "msw_qsim_*_L1.IDF"
+    ).isel(layer=0, drop=True)
+    hgw_ref = imod.idf.open(
+        modstrip_full_range_dbase_loc / "results" / "msw_Hgw" / "msw_Hgw_*_L1.IDF"
+    ).isel(layer=0, drop=True)
 
     # read mf6 output
     head_mf6 = imod.mf6.open_hds(headfile, grbfile, False)
     flux_mf6 = imod.mf6.open_cbc(cbcfile, grbfile, False)
 
     # evaluation criterion
-    criterion_q = 0.001  # %          ! compare waterbalanse of run
-    criterion_q_ref = 0.0001  # %      ! compare to reference
+    criterion_wbal = 0.0002  # mm
+    criterion_head = 0.0001  # mm
+    criterion_q = 0.0001  # %
 
-    # log array
-    log = np.empty(371 * 7, dtype=bool).reshape((7, 371))
-    for isvat in np.arange(371):
-        print("reading files svat {isvat}".format(isvat=isvat))
-        # read msw output and reference data
-        msw_csv = (
-            tmp_path / "msw" / "msw" / "csv" / "svat_dtgw_{:010d}.csv".format(isvat + 1)
-        )
-        msw_csv_regression = (
-            modstrip_full_range_dbase_loc
-            / "results"
-            / "svat_dtgw_{:010d}.csv".format(isvat + 1)
-        )
+    # first write to logfile
+    log = np.empty(372 * 7, dtype=float).reshape((7, 372))
 
-        data_2023_regression = pd.read_csv(msw_csv_regression, skipinitialspace=True)
-        assert msw_csv.exists()
-        assert msw_csv.stat().st_size > 0
-        data_develop = pd.read_csv(msw_csv, skipinitialspace=True)
+    # CHECK 1: evaluate absolute value of qsim to reference
+    # if assert exception; metaswap internal flux is changed, possible conceptual changes in MetaSwap
+    log[0, :] = total_flux_error(qmsw, qmsw_ref)
 
-        # convert to coupling balans terms
-        qmodf = data_develop["qmodf(mm)"] / 1000 * data_develop["area(m2)"]
-        qmsw = data_develop["qsim(mm)"] / 1000 * data_develop["area(m2)"]
-        qmsw_cor = data_develop["qsimcorrmf(mm)"] / 1000 * data_develop["area(m2)"]
-        dhgw = data_develop["dHgw(m)"]
-        hgw = data_develop["Hgw(m)"]
-        hgw_mod = data_develop["Hgwmodf(m)"]
-        sc1 = data_develop["sc1(m3/m2/m)"] * data_develop["area(m2)"]
+    # CHECK 2: evaluate absolute value of qmodf to reference
+    # if assert exception; modflow internal flux is changed, possible conceptual changes in MODFLOW
+    log[1, :] = total_flux_error(qmodf, qmodf_ref)
 
-        # reference data
-        qmodf_ref = (
-            data_2023_regression["qmodf(mm)"] / 1000 * data_2023_regression["area(m2)"]
-        )
-        qmsw_ref = (
-            data_2023_regression["qsim(mm)"] / 1000 * data_2023_regression["area(m2)"]
-        )
-        hgw_ref = data_2023_regression["Hgw(m)"]
+    # CHECK 3: evaluate full coupling balance
+    dh_insert = head_mf6.isel(layer=0, time=0).values - -5.0
+    dhgw = np.insert(
+        np.diff(head_mf6.isel(layer=0).values, axis=0), 0, dh_insert, axis=0
+    )
+    lhs = (qmodf + qmsw) * 1000  # mm
+    rhs = (dhgw * sc1.values) * 1000  # mm
+    log[2, :] = np.squeeze(np.amax(np.abs(lhs.values - rhs), axis=0))
 
-        # --- check coupling concept ---
+    # CHECK 4: evaluate if non-convergentie is corrected by correction flux
+    # we cant use the difference betwee hgw and hgw_mod, check with Paul how to evaluate this
 
-        # coupling balanse of MF6-MSW is defined as:
-        # qsim + qmodf = dHgw * sc1
+    # CHECK 5: evaluate absolute value of hgw to reference
+    log[3, :] = np.squeeze(np.amax(np.abs(hgw.values - hgw_ref.values), axis=0))
 
-        # CHECK 1: evaluate absolute value of qsim to reference
-        # if assert exception; metaswap internal flux is changed, possible conceptual changes in MetaSwap
-        log[0, isvat] = total_flux_error(qmsw, qmsw_ref)
+    # --- check correctness of coupling ---
 
-        # CHECK 2: evaluate absolute value of qmodf to reference
-        # if assert exception; modflow internal flux is changed, possible conceptual changes in MODFLOW
-        log[1, isvat] = total_flux_error(qmodf, qmodf_ref)
+    # CHECK 1: evaluate if sum of RCH (mf6) is equal to qmsw (msw)
+    log[4, :] = total_flux_error(flux_mf6["rch_msw"].isel(layer=0) / (100 * 100), qmsw)
 
-        # CHECK 3: evaluate full coupling balance
-        lhs = (qmodf + qmsw) / data_develop["area(m2)"] * 1000  # mm
-        rhs = (dhgw * sc1) / data_develop["area(m2)"] * 1000  # mm
+    # CHECK 2: evaluate if head of mf6-cel is equal to head of msw-svat (this cases 1-1 coupling)
+    log[5, :] = np.squeeze(
+        np.amax(np.abs(head_mf6.isel(layer=0).values - hgw.values), axis=0)
+    )
 
-        log[2, isvat] = total_flux_error(lhs, rhs) < criterion_q
-
-        # CHECK 4: evaluate if non-convergentie is corrected by correction flux
-        # we cant use the difference betwee hgw and hgw_mod, check with Paul how to evaluate this
-
-        # CHECK 5: evaluate absolute value of hgw to reference
-        log[3, isvat] = total_flux_error(hgw, hgw_ref)
-
-        # --- check correctness of coupling ---
-
-        # CHECK 1: evaluate if sum of RCH (mf6) is equal to qmsw (msw)
-        log[4, isvat] = total_flux_error(
-            flux_mf6["rch_msw"][:, 0, isvat, 0].values, qmsw
-        )
-
-        # CHECK 2: evaluate if head of mf6-cel is equal to head of msw-svat (this cases 1-1 coupling)
-        log[5, isvat] = total_flux_error(head_mf6[:, 0, isvat, 0].values, hgw)
-
-        # CHECK 3: evaluate if STO (mf6) is equal to SC1 (MSW)
-        # MF6 does not have STO as output variable, so we compute it from Q-STO and dH
-        dh_insert = -5.0 - head_mf6[0, 0, isvat, 0]  # dh relative to ic
-        dh_mf6 = np.insert(-np.diff(head_mf6[:, 0, isvat, 0].values), 0, dh_insert)
-        sc1_mf6 = flux_mf6["sto-ss"][:, 0, isvat, 0].values / (dh_mf6 * 100**2)
-        log[6, isvat] = total_flux_error(data_develop["sc1(m3/m2/m)"], sc1_mf6)
+    # CHECK 3: evaluate if STO (mf6) is equal to SC1 (MSW)
+    # MF6 does not have STO as output variable, so we compute it from Q-STO and dH
+    sc1_mf6 = -flux_mf6["sto-ss"].isel(layer=0).values / (dhgw * 100**2)
+    log[6, :] = np.squeeze(np.amax(np.abs(sc1.values - sc1_mf6), axis=0))
 
     # write logfile
-    np.savetxt(log_path, log, fmt="%5i", newline="\n")
+    np.savetxt(log_path, log, fmt="%1.5f", newline="\n")
 
-    assert any(log)
+    # do asserts
+    assert any(log[0, :] < criterion_q)
+    assert any(log[1, :] < criterion_q)
+    assert any(log[2, :] < criterion_wbal)
+    assert any(log[3, :] < criterion_head)
+    assert any(log[4, :] < criterion_q)
+    assert any(log[5, :] < criterion_head)
+    assert any(log[6, :] < criterion_head)
