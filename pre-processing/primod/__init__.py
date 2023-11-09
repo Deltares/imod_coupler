01286ab4bdb3be5c4815205ce946ad3dae5788ea
Hendrik Kok <hendrik.kok@deltares.nl>
2023-11-09 15:27:07 +0100

    add RibaMetaMod to premod

--- pre-processing/primod/__init__.py
+++ pre-processing/primod/__init__.py
@@ -1,6 +1,7 @@
 from primod.metamod import MetaMod
+from primod.ribametamod import RibaMetaMod
 from primod.ribamod import RibaMod
 
-__all__ = ["MetaMod", "RibaMod"]
+__all__ = ["MetaMod", "RibaMod", "RibaMetaMod"]
 
 __version__ = "0.2.0-dev"
