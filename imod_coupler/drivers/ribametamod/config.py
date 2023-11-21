9557d6e8e2e18d9dd5bf3bfed65a39f52048722c
rleander <Robert.Leander@deltares.nl>
2023-11-21 15:31:47 +0100

    Julian's review comment on Optional versus | None

--- imod_coupler/drivers/ribametamod/config.py
+++ imod_coupler/drivers/ribametamod/config.py
@@ -19,20 +19,18 @@ class Coupling(BaseModel):
     mf6_active_drainage_packages: Dict[str, str]
     mf6_passive_river_packages: Dict[str, str]
     mf6_passive_drainage_packages: Dict[str, str]
-    mf6_msw_mappings: Optional[Dict[str, str]] = None
+    mf6_msw_mappings: Dict[str, str] | None = None
 
     enable_sprinkling: Optional[bool] = False  # true whemn sprinkling is active
-    mf6_msw_recharge_pkg: Optional[
-        str
-    ] = None  # the recharge package that will be used for coupling
-    mf6_msw_well_pkg: (
-        str | None
-    ) = None  # the well package that will be used for coupling when sprinkling is active
-    mf6_msw_node_map: Optional[FilePath] = None  # the path to the node map file
-    mf6_msw_recharge_map: Optional[FilePath] = None  # the pach to the recharge map file
-    mf6_msw_sprinkling_map: Optional[
-        FilePath
-    ] = None  # the path to the sprinkling map file
+    mf6_msw_recharge_pkg: str | None = (
+        None  # the recharge package that will be used for coupling
+    )
+    mf6_msw_well_pkg: str | None = None  # the well package that will be used for coupling when sprinkling is active
+    mf6_msw_node_map: FilePath | None = None  # the path to the node map file
+    mf6_msw_recharge_map: FilePath | None = None  # the pach to the recharge map file
+    mf6_msw_sprinkling_map: FilePath | None = (
+        None  # the path to the sprinkling map file
+    )
     output_config_file: FilePath | None = None
 
     @validator("mf6_msw_well_pkg")
