ae76f3f6728e7feb3b4c9ccc586b2237ccf86efe
Hendrik Kok <hendrik.kok@deltares.nl>
2023-11-21 17:17:19 +0100

    linter + ribametamod-test

--- tests/test_ribametamod.py
+++ tests/test_ribametamod.py
@@ -1,20 +1,12 @@
 import shutil
-import subprocess
 from pathlib import Path
-from typing import Any, Union
+from typing import Union
 
-import numpy as np
-import pandas as pd
-import pytest
 import tomli_w
-from numpy.testing import assert_allclose
-from primod.ribamod import RibaMod
-from pytest_cases import parametrize_with_cases
 
 from imod_coupler.__main__ import run_coupler
 
 
-# @parametrize_with_cases("ribamod_model")
 def test_metamod(
     tmp_path_dev: Path,
     modflow_dll_devel: Path,
@@ -30,7 +22,7 @@ def test_metamod(
 
     shutil.copytree(bucket_ribametamod_loc, path_dev)
 
-    toml_path = path_dev / "imod_coupler_metamod.toml"
+    toml_path = path_dev / "imod_coupler.toml"
     modflow6_dll = modflow_dll_devel
     modflow6_model_dir = path_dev / "modflow6"
     metaswap_dll = metaswap_dll_devel
@@ -41,7 +33,6 @@ def test_metamod(
 
     write_metamod_toml(
         path_dev,
-        "imod_coupler_metamod.toml",
         modflow6_dll,
         modflow6_model_dir,
         metaswap_dll,
@@ -65,13 +56,12 @@ def test_ribamod(
 
     shutil.copytree(bucket_ribametamod_loc, path_dev)
 
-    toml_path = path_dev / "imod_coupler_ribamod.toml"
+    toml_path = path_dev / "imod_coupler.toml"
     modflow6_dll = modflow_dll_devel
     modflow6_model_dir = path_dev / "modflow6"
 
     write_ribamod_toml(
         path_dev,
-        "imod_coupler_ribamod.toml",
         modflow6_dll,
         modflow6_model_dir,
         ribasim_dll_devel,
@@ -81,9 +71,48 @@ def test_ribamod(
     run_coupler(toml_path)
 
 
+def test_ribametamod(
+    tmp_path_dev: Path,
+    modflow_dll_devel: Path,
+    metaswap_dll_devel: Path,
+    metaswap_dll_dep_dir_devel: Path,
+    metaswap_lookup_table: Path,
+    ribasim_dll_devel: Path,
+    ribasim_dll_dep_dir_devel: Path,
+    bucket_ribametamod_loc: Path,
+) -> None:
+    """
+    Test if coupled ribametamod models run with the iMOD Coupler development version.
+    """
+    path_dev = tmp_path_dev / "bucket_model_driver_ribametamod"
+
+    shutil.copytree(bucket_ribametamod_loc, path_dev)
+
+    fill_para_sim_template(path_dev / "metaswap", metaswap_lookup_table)
+
+    toml_path = path_dev / "imod_coupler.toml"
+    modflow6_dll = modflow_dll_devel
+    modflow6_model_dir = path_dev / "modflow6"
+    metaswap_dll = metaswap_dll_devel
+    metaswap_dll_dependency = metaswap_dll_dep_dir_devel
+    metaswap_model_dir = path_dev / "metaswap"
+
+    write_ribametamod_toml(
+        path_dev,
+        modflow6_dll,
+        modflow6_model_dir,
+        metaswap_dll,
+        metaswap_dll_dependency,
+        metaswap_model_dir,
+        ribasim_dll_devel,
+        ribasim_dll_dep_dir_devel,
+        path_dev / "ribasim" / "ribasim.toml",
+    )
+    # run_coupler(toml_path) # don't run until optional branch is merged
+
+
 def write_metamod_toml(
     tmp_path_dev: Union[str, Path],
-    toml_name: str,
     modflow6_dll: Union[str, Path],
     modflow6_model_dir: Union[str, Path],
     metaswap_dll: Union[str, Path],
@@ -139,13 +168,12 @@ def write_metamod_toml(
             ],
         },
     }
-    with open(tmp_path_dev / toml_name, "wb") as f:
+    with open(tmp_path_dev / "imod_coupler.toml", "wb") as f:
         tomli_w.dump(coupler_toml, f)
 
 
 def write_ribamod_toml(
     tmp_path_dev: Union[str, Path],
-    toml_name: str,
     modflow6_dll: Union[str, Path],
     modflow6_model_dir: Union[str, Path],
     ribasim_dll: Union[str, Path],
@@ -193,17 +221,88 @@ def write_ribamod_toml(
             "coupling": [
                 {
                     "mf6_model": "GWF_1",
+                    "mf6_active_river_packages": {"riv-1": "exchanges/riv-1.tsv"},
+                    "mf6_passive_river_packages": {},
+                    "mf6_active_drainage_packages": {},
+                    "mf6_passive_drainage_packages": {},
                 }
             ],
-            "coupling": {
-                "mf6_active_river_packages": {"riv-1": "exchanges/riv-1.tsv"},
-                "mf6_passive_river_packages": {},
-                "mf6_active_drainage_packages": {},
-                "mf6_passive_drainage_packages": {},
-            },
         },
     }
-    with open(tmp_path_dev / toml_name, "wb") as f:
+    with open(tmp_path_dev / "imod_coupler.toml", "wb") as f:
+        tomli_w.dump(coupler_toml, f)
+
+
+def write_ribametamod_toml(
+    tmp_path_dev: Union[str, Path],
+    modflow6_dll: Union[str, Path],
+    modflow6_model_dir: Union[str, Path],
+    metaswap_dll: Union[str, Path],
+    metaswap_dll_dependency: Union[str, Path],
+    metaswap_model_dir: Union[str, Path],
+    ribasim_dll: Union[str, Path],
+    ribasim_dll_dependency: Union[str, Path],
+    ribasim_config_file: Union[str, Path],
+) -> None:
+    """
+    Write .toml file which configures the imod coupler run.
+    Parameters
+    ----------
+    directory: str or Path
+        Directory in which to write the .toml file.
+    modflow6_dll: str or Path
+        Path to modflow6 .dll. You can obtain this library by downloading
+        `the last iMOD5 release
+        <https://oss.deltares.nl/web/imod/download-imod5>`_
+    metaswap_dll: str or Path
+        Path to metaswap .dll. You can obtain this library by downloading
+        `the last iMOD5 release
+        <https://oss.deltares.nl/web/imod/download-imod5>`_
+    metaswap_dll_dependency: str or Path
+        Directory with metaswap .dll dependencies. Directory should contain:
+        [fmpich2.dll, mpich2mpi.dll, mpich2nemesis.dll, TRANSOL.dll]. You
+        can obtain these by downloading `the last iMOD5 release
+        <https://oss.deltares.nl/web/imod/download-imod5>`_
+    coupling_dict: dict
+        Dictionary with names of coupler packages and paths to mappings.
+    """
+    coupler_toml = {
+        "timing": False,
+        "log_level": "INFO",
+        "driver_type": "metamod",
+        "driver": {
+            "kernels": {
+                "modflow6": {
+                    "dll": str(modflow6_dll),
+                    "work_dir": str(modflow6_model_dir),
+                },
+                "metaswap": {
+                    "dll": str(metaswap_dll),
+                    "work_dir": str(metaswap_model_dir),
+                    "dll_dep_dir": str(metaswap_dll_dependency),
+                },
+                "ribasim": {
+                    "dll": str(ribasim_dll),
+                    "dll_dep_dir": str(ribasim_dll_dependency),
+                    "config_file": str(ribasim_config_file),
+                },
+            },
+            "coupling": [
+                {
+                    "enable_sprinkling": False,
+                    "mf6_model": "GWF_1",
+                    "mf6_msw_recharge_pkg": "rch_msw",
+                    "mf6_msw_node_map": "exchanges/nodenr2svat.dxc",
+                    "mf6_msw_recharge_map": "exchanges/rchindex2svat.dxc",
+                    "mf6_active_river_packages": {"riv-1": "exchanges/riv-1.tsv"},
+                    "mf6_passive_river_packages": {},
+                    "mf6_active_drainage_packages": {},
+                    "mf6_passive_drainage_packages": {},
+                },
+            ],
+        },
+    }
+    with open(tmp_path_dev / "imod_coupler.toml", "wb") as f:
         tomli_w.dump(coupler_toml, f)
 
 
