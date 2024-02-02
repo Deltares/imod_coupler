046cf9b279c712878c0dab8fe085bd5bb4afdcd5
Hendrik Kok <hendrik.kok@deltares.nl>
2024-02-02 15:25:13 +0100

    Add all RibaMod tests to RibaMetaMod tests

--- tests/test_ribametamod_cases.py
+++ tests/test_ribametamod_cases.py
@@ -1,61 +1,92 @@
 import geopandas as gpd
 import ribasim
-from imod.mf6 import GroundwaterFlowModel, Modflow6Simulation, River
+from imod.mf6 import GroundwaterFlowModel, Modflow6Simulation, River, Recharge
 from imod.msw import MetaSwapModel
 from primod.ribamod import DriverCoupling, RibaMod
+import xarray as xr
+from test_ribamod_cases import get_mf6_river_packagenames, get_mf6_drainage_packagenames, create_basin_definition, get_mf6_gwf_modelnames
 
 
-def create_basin_definition(
-    ribasim_model: ribasim.Model, buffersize: float
-) -> gpd.GeoDataFrame:
-    node = ribasim_model.network.node.df
-    basin_nodes = ribasim_model.basin.static.df["node_id"].unique()
-    basin_geometry = node.loc[basin_nodes].geometry
-    # Call to_numpy() to get rid of the index
-    basin_definition = gpd.GeoDataFrame(
-        data={"node_id": basin_nodes},
-        geometry=basin_geometry.buffer(buffersize).to_numpy(),
-    )
-    return basin_definition
-
-
-def get_mf6_gwf_modelnames(
-    mf6_simulation: Modflow6Simulation,
-) -> list[tuple[str, GroundwaterFlowModel]]:
+def add_rch_package(
+    mf6_model: Modflow6Simulation,
+) -> Modflow6Simulation:
     """
-    Get names of gwf models in mf6 simulation
+    adds recharge package to MODFLOW6 model for coupling with MetaSWAP
     """
-    return [
-        (key, value)
-        for key, value in mf6_simulation.items()
-        if isinstance(value, GroundwaterFlowModel)
-    ]
-
-
-def get_mf6_river_packagenames(mf6_model: GroundwaterFlowModel) -> list[str]:
-    """
-    Get names of river packages in mf6 simulation
-    """
-    return [key for key, value in mf6_model.items() if isinstance(value, River)]
+    idomain = mf6_model["GWF_1"]["dis"]['idomain']
+    recharge = xr.zeros_like(idomain.sel(layer=1), dtype=float)
+    recharge = recharge.where(idomain.sel(layer=1))
+    mf6_model["GWF_1"]['rch_msw'] = Recharge(rate = recharge)
+    return mf6_model
 
 
 def case_bucket_model(
-    mf6_bucket_model: Modflow6Simulation,
-    msw_bucket_model: MetaSwapModel,
-    ribasim_bucket_model: ribasim.Model,
-) -> RibaMod:
-    mf6_modelname, mf6_model = get_mf6_gwf_modelnames(mf6_bucket_model)[0]
-    mf6_active_river_packages = get_mf6_river_packagenames(mf6_model)
-    basin_definition = create_basin_definition(ribasim_bucket_model, buffersize=10.0)
+   mf6_bucket_model: Modflow6Simulation,
+   msw_bucket_model: MetaSwapModel,
+   ribasim_bucket_model: ribasim.Model,
+)-> RibaMod | MetaSwapModel:
+   mf6_modelname, mf6_model = get_mf6_gwf_modelnames(mf6_bucket_model)[0]
+   mf6_active_river_packages = get_mf6_river_packagenames(mf6_model)
+   basin_definition = create_basin_definition(ribasim_bucket_model, buffersize=10.0)
+   mf6_bucket_model = add_rch_package(mf6_bucket_model)
 
+   driver_coupling = DriverCoupling(
+       mf6_model=mf6_modelname,
+       mf6_active_river_packages=mf6_active_river_packages,
+   )
+   # should becomde RibaMetaMod-class, including the MetaSWAP model
+   return RibaMod(
+       ribasim_model=ribasim_bucket_model,
+       mf6_simulation=mf6_bucket_model,
+       basin_definition=basin_definition,
+       coupling_list=[driver_coupling],
+   ), msw_bucket_model 
+
+
+def case_backwater_model(
+    mf6_backwater_model: Modflow6Simulation,
+    msw_backwater_model: MetaSwapModel,
+    ribasim_backwater_model: ribasim.Model,
+) -> RibaMod | MetaSwapModel:
+    mf6_modelname, mf6_model = get_mf6_gwf_modelnames(mf6_backwater_model)[0]
+    mf6_active_river_packages = get_mf6_river_packagenames(mf6_model)
+    mf6_active_drainage_packages = get_mf6_drainage_packagenames(mf6_backwater_model)
+    basin_definition = create_basin_definition(ribasim_backwater_model, buffersize=5.0)
+    mf6_backwater_model = add_rch_package(mf6_backwater_model)
+    
+    driver_coupling = DriverCoupling(
+        mf6_model=mf6_modelname,
+        mf6_active_river_packages=mf6_active_river_packages,
+        mf6_active_drainage_packages=mf6_active_drainage_packages,
+    )
+   # should becomde RibaMetaMod-class, including the MetaSWAP model
+    return RibaMod(
+        ribasim_model=ribasim_backwater_model,
+        mf6_simulation=mf6_backwater_model,
+        basin_definition=basin_definition,
+        coupling_list=[driver_coupling],
+    ), msw_backwater_model
+
+   
+def case_two_basin_model(
+    mf6_two_basin_model: Modflow6Simulation,
+    msw_two_basin_model: MetaSwapModel,
+    ribasim_two_basin_model: ribasim.Model,
+) -> RibaMod | MetaSwapModel:
+    mf6_modelname, mf6_model = get_mf6_gwf_modelnames(mf6_two_basin_model)[0]
+    mf6_active_river_packages = get_mf6_river_packagenames(mf6_model)
+    basin_definition = create_basin_definition(
+        ribasim_two_basin_model, buffersize=250.0
+    )
+    mf6_two_basin_model = add_rch_package(mf6_two_basin_model)
     driver_coupling = DriverCoupling(
         mf6_model=mf6_modelname,
         mf6_active_river_packages=mf6_active_river_packages,
     )
-
+   # should becomde RibaMetaMod-class, including the MetaSWAP model
     return RibaMod(
-        ribasim_model=ribasim_bucket_model,
-        mf6_simulation=mf6_bucket_model,
+        ribasim_model=ribasim_two_basin_model,
+        mf6_simulation=mf6_two_basin_model,
         basin_definition=basin_definition,
         coupling_list=[driver_coupling],
-    ), msw_bucket_model
+    ), msw_two_basin_model
