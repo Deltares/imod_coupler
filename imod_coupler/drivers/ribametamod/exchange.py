039d8b3cfcb871d8afe3a1b40d7d066cbad6f553
Hendrik Kok <hendrik.kok@deltares.nl>
2024-02-09 10:48:09 +0100

    update exchange class to general form to be used in RibaMod and RibaMetaMod

--- imod_coupler/drivers/ribametamod/exchange.py
+++ imod_coupler/drivers/ribametamod/exchange.py
@@ -5,68 +5,80 @@ from numpy.typing import NDArray
 from ribasim_api import RibasimApi
 
 
-class exchange_balance_1d:
-    predicted: dict[str, NDArray[np.float_]]
-    realised: dict[str, NDArray[np.float_]]
+class exchange_balance:
+    demands: dict[str, NDArray[np.float_]]
+    demands_negative: dict[str, NDArray[np.float_]]
+    realised_negative: dict[str, NDArray[np.float_]]
     dim: int
     sum_keys: list[str]
 
-    def __init__(self, dim: int) -> None:
+    def __init__(self, dim: int, labels: list[str], shortage_label = str) -> None:
         self.dim = dim
-        self.sum_keys = []
-        self.predicted = {
-            "sum": np.zeros(shape=self.dim, dtype=np.float_),
-        }
-        self.realised = {
-            "sum": np.zeros(shape=self.dim, dtype=np.float_),
-        }
+        self.flux_labels = labels
+        self.shortage_label = shortage_label
+        self.demand = self._zeros_array()
+        self._init_arrays()
+
+    def set_realised(
+        self, realised: NDArray[np.float_]
+    ) -> None:
+        """
+        This function computes the realised volumes
+        """
+        shortage = np.absolute(self.demand - realised)
+        demand_negative = self.demand_negative
+        self._check_valid_shortage(shortage, demand_negative)
+        realised_fraction = 1 - (- shortage / demand_negative)
+        for flux_label in self.flux_labels:
+            self.realised_negative[flux_label] =  self.demands_negative[flux_label] * realised_fraction 
+                 
+    def _check_valid_shortage(
+        self, shortage: NDArray[np.float_], demand_negative: NDArray[np.float_]
+    ) -> ValueError | None:
+        if not np.any(np.logical_and(self.demand > 0.0, shortage > 0.0)):
+            raise ValueError("Invalid realised values: found shortage for positive demand")
+        if np.any(shortage > np.absolute(demand_negative)):
+            raise ValueError("Invalid realised values: found shortage larger than negative demand contributions")
 
     def reset(self) -> None:
         """
-        function initialses the 2 water-balance dicts and sets all arrays at 0.
-        The dimension of the arrays is based on the shape of dflow 1d nodes
+        function sets all arrays to zero
         """
-        self.predicted = {
-            "msw_ponding2riba_flux": np.zeros(shape=self.dim, dtype=np.float_),
-            "msw_sprinkling2riba_flux": np.zeros(shape=self.dim, dtype=np.float_),
-            "sum": np.zeros(shape=self.dim, dtype=np.float_),
-        }
-        for val in self.predicted.values():
-            val[:] = np.zeros(shape=self.dim, dtype=np.float_)
+        for flux_label in self.flux_labels:
+            self.demands[flux_label][:] = 0.0
+            self.demands_negative[flux_label][:] = 0.0
+            self.realised_negative[flux_label][:] = 0.0
 
-        for val in self.realised.values():
-            val[:] = np.zeros(shape=self.dim, dtype=np.float_)
-
-    def sum_demands(self) -> None:
+    def _zeros_array(self) -> NDArray[np.float_]:
+        return np.zeros(shape=self.dim, dtype=np.float_)
+    
+    def _init_arrays(self) -> None:
+        self.demands = {}
+        self.demands_negative = {}
+        self.realised_negative = {}
+        for flux_label in self.flux_labels:
+            self.demands[flux_label] = self._zeros_array()
+            self.demands_negative[flux_label] = self._zeros_array()
+            self.realised_negative[flux_label] = self._zeros_array()
+            
+    @property
+    def demand_negative(self) -> NDArray[np.float_]:
         """
-        function calculates the sum of all arrays in self.predicted dict.
-        The calculated flux is stored under the key "sum". This flux is used to send to ribasim
+        compute negative demand as sum of demands arrays
         """
-        sum_dict = {key: self.predicted[key] for key in self.sum_keys}
-        sum_array = np.stack(list(sum_dict.values()))
-        self.predicted["sum"][:] = sum_array.sum(axis=0)
-
-    def set_realised_no_shortage(
-        self, sum_from_sw: NDArray[np.float_], sum_to_sw: NDArray[np.float_]
-    ) -> None:
+        sum_array = np.stack(list(self.demands_negative.values()))
+        return sum_array.sum(axis=0)
+        
+    @property
+    def demand(self) -> NDArray[np.float_]:
         """
-        This function sets the realised array elements on predicted values for cases where no shortage is computed.
-        There is no shortage at elements where the flux realised by the surface water component >= than the flux send to the surface water component.
-
-        Parameters
-        ----------
-        sum_from_sw : np.float_
-            flux realised by the surface water component
-        sum_to_sw : np.float_
-            flux send to the surface water component
+        compute demand as sum of demands arrays
         """
-        condition = np.greater_equal(sum_to_sw, sum_from_sw)
-        for key in self.predicted.keys():
-            if key in self.realised:
-                self.realised[key][condition] = self.predicted[key][condition]
+        sum_array = np.stack(list(self.demands.values()))
+        return sum_array.sum(axis=0)
+        
 
-
-class exchange_ribasim_1d(exchange_balance_1d):
+class exchange_ribasim_1d(exchange_balance):
     ribasim_infiltration: NDArray[Any]
     ribasim_drainage: NDArray[Any]
 
@@ -75,8 +87,8 @@ class exchange_ribasim_1d(exchange_balance_1d):
         self.ribasim_drainage = self.riba.get_value_ptr("drainage")
         self.ribasim_infiltration = self.riba.get_value_ptr("infiltration")
         self.ribasim_level = self.riba.get_value_ptr("level")
-        exchange_balance_1d.__init__(self, np.size(self.ribasim_drainage))
-        self.predicted["msw_ponding2riba_flux"] = np.zeros(
+        exchange_balance.__init__(self, np.size(self.ribasim_drainage))
+        self.demand["msw_ponding2riba_flux"] = np.zeros(
             shape=self.dim, dtype=np.float_
         )
         self.realised["msw_ponding2riba_flux"] = np.zeros(
@@ -86,11 +98,11 @@ class exchange_ribasim_1d(exchange_balance_1d):
     def to_ribasim(self) -> None:
         if self.ribasim_drainage is not None:
             self.ribasim_drainage[:] = np.where(
-                self.predicted["sum"][:] > 0, self.predicted["sum"][:], 0.0
+                self.demand["sum"][:] > 0, self.demand["sum"][:], 0.0
             )
         if self.ribasim_infiltration is not None:
             self.ribasim_infiltration[:] = np.where(
-                self.predicted["sum"][:] < 0, self.predicted["sum"][:], 0.0
+                self.demand["sum"][:] < 0, self.demand["sum"][:], 0.0
             )
 
     def sum_demands(self) -> None:
