582dfd7050a87a2d20f2392e2acec5ebd26be805
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-02-26 18:37:44 +0100

    Add tests for recharge mapping

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -1,13 +1,16 @@
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_equal
+import pytest
 from pytest_cases import parametrize_with_cases
 
 from imod_coupler.utils import create_mapping
+from imod import mf6
+from imod import msw
 
+from primod.mapping.rch_svat_mapping import RechargeSvatMapping
 
 @parametrize_with_cases(
-    "src_idx,tgt_idx,nsrc,ntgt,operator,expected_map_dense,expected_mask"
-)
+    "src_idx,tgt_idx,nsrc,ntgt,operator,expected_map_dense,expected_mask", prefix="util_")
 def test_create_mapping(
     src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 ):
@@ -27,3 +30,31 @@ def test_create_mapping(
 
     assert_almost_equal(map_out.toarray(), expected_map_dense)
     assert_array_equal(mask, expected_mask)
+
+
+@parametrize_with_cases(
+    "recharge", prefix="rch", has_tag="succeed")
+def test_recharge_mapping(
+    recharge: mf6.Recharge, prepared_msw_model: msw.MetaSwapModel
+):
+    """
+    Test Recharge package validation
+    """
+    _, svat = prepared_msw_model["grid"].generate_index_array()
+
+    rch_svat_mapping = RechargeSvatMapping(svat, recharge)
+    
+    assert np.all(rch_svat_mapping.dataset["layer"] == 1)
+    assert np.all(rch_svat_mapping.dataset["svat"] == svat)
+    assert rch_svat_mapping.dataset["rch_id"].max() == 12
+    assert rch_svat_mapping.dataset["rch_active"].sum() == 12
+
+@parametrize_with_cases(
+    "recharge", prefix="rch", has_tag="fail")
+def test_recharge_mapping_fail(
+    recharge: mf6.Recharge, prepared_msw_model: msw.MetaSwapModel
+):
+    _, svat = prepared_msw_model["grid"].generate_index_array()
+
+    with pytest.raises(ValueError):
+        RechargeSvatMapping(svat, recharge)
--- tests/test_mapping_cases.py
+++ tests/test_mapping_cases.py
@@ -1,7 +1,8 @@
 import numpy as np
+import xarray as xr
+from pytest_cases import case
 
-
-def case_1_1_symmetric_sum():
+def util_1_1_symmetric_sum():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([0, 1, 2], dtype=int)
 
@@ -20,7 +21,7 @@ def case_1_1_symmetric_sum():
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 
 
-def case_1_1_symmetric_avg():
+def util_1_1_symmetric_avg():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([0, 1, 2], dtype=int)
 
@@ -39,7 +40,7 @@ def case_1_1_symmetric_avg():
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 
 
-def case_1_1_asymmetric_sum():
+def util_1_1_asymmetric_sum():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([2, 3, 4], dtype=int)
 
@@ -61,7 +62,7 @@ def case_1_1_asymmetric_sum():
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 
 
-def case_1_1_asymmetric_avg():
+def util_1_1_asymmetric_avg():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([2, 3, 4], dtype=int)
 
@@ -83,7 +84,7 @@ def case_1_1_asymmetric_avg():
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 
 
-def case_n_1_symmetric_sum():
+def util_n_1_symmetric_sum():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([0, 1, 1], dtype=int)
 
@@ -102,7 +103,7 @@ def case_n_1_symmetric_sum():
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 
 
-def case_n_1_symmetric_avg():
+def util_n_1_symmetric_avg():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([0, 1, 1], dtype=int)
 
@@ -121,7 +122,7 @@ def case_n_1_symmetric_avg():
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 
 
-def case_n_1_asymmetric_sum():
+def util_n_1_asymmetric_sum():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([2, 2, 4], dtype=int)
 
@@ -143,7 +144,7 @@ def case_n_1_asymmetric_sum():
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
 
 
-def case_n_1_asymmetric_avg():
+def util_n_1_asymmetric_avg():
     src_idx = np.array([0, 1, 2], dtype=int)
     tgt_idx = np.array([2, 2, 4], dtype=int)
 
@@ -163,3 +164,44 @@ def case_n_1_asymmetric_avg():
         ]
     )
     return src_idx, tgt_idx, nsrc, ntgt, operator, expected_map_dense, expected_mask
+
+@case(tags='succeed')
+def rch_2D(recharge):
+    """
+    Case where recharge is a 2D grid (y,x) and has a "layer" coord.
+    """
+    return recharge
+
+@case(tags='succeed')
+def rch_3D_layer_size_1(recharge):
+    """
+    Case where recharge has a third dimension "layer" with size one.
+    """
+    ds = recharge.dataset.expand_dims("layer").assign_coords(layer=[1])
+    recharge.dataset = ds
+    return recharge
+
+@case(tags='fail')
+def rch_3D(recharge):
+    """
+    Case where recharge assigned to multiple layers.
+    """
+    ds = recharge.dataset.drop_vars("layer")
+    layer_data = xr.DataArray([1, 1, 1], coords={"layer": [1, 2, 3]}, dims=("layer",))
+    ds["rate"] = layer_data * ds["rate"]
+    recharge.dataset = ds
+    return recharge
+
+@case(tags='fail')
+def rch_transient(recharge):
+    """
+    Case where rate has time dimension, should fail because mapping cannot
+    change through time.
+    """
+    ds = recharge.dataset
+    times_str = ["2000-01-01", "2000-01-02", "2000-01-03"]
+    times = [np.datetime64(t) for t in times_str]
+    time_data = xr.DataArray([1, 1, 1], coords={"time": times}, dims=("time",))
+    ds["rate"] = time_data * ds["rate"]
+    recharge.dataset = ds
+    return recharge
