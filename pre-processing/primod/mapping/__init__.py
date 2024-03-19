c5118f36c0145cf8d5873733a84ce5e797267091
Huite Bootsma <huitebootsma@gmail.com>
2024-03-19 15:18:06 +0100

    Export DriverCoupling. Move mappings one level up for ease of import in tests

---
+++ pre-processing/primod/mapping/__init__.py
@@ -0,0 +1,13 @@
+from primod.mapping.node_basin_mapping import NodeBasinMapping
+from primod.mapping.node_svat_mapping import NodeSvatMapping
+from primod.mapping.rch_svat_mapping import RechargeSvatMapping
+from primod.mapping.svat_basin_mapping import SvatBasinMapping
+from primod.mapping.wel_svat_mapping import WellSvatMapping
+
+__all__ = (
+    "NodeBasinMapping",
+    "NodeSvatMapping",
+    "RechargeSvatMapping",
+    "SvatBasinMapping",
+    "WellSvatMapping",
+)
