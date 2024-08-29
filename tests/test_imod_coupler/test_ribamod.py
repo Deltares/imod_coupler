8715587e58e8ddb16807b7d7eeb0c2e1e12bac77
HendrikKok <59684726+HendrikKok@users.noreply.github.com>
2024-08-29 16:29:20 +0200

    update Backwater test to correct budget terms (#326)

--- tests/test_imod_coupler/test_ribamod.py
+++ tests/test_imod_coupler/test_ribamod.py
@@ -167,12 +167,7 @@ def test_ribamod_backwater(
     volume_balance = results.basin_df[results.basin_df["time"] == "2020-12-31"]
     # Check's what lost and gained in the basins
     ribasim_budget = (
-        (
-            volume_balance["inflow_rate"]
-            - volume_balance["outflow_rate"]
-            - volume_balance["balance_error"]
-        )[:-1]
-        * 86_400.0
+        (volume_balance["infiltration"] - volume_balance["drainage"])[:-1] * 86_400.0
     ).to_numpy()
     # It seems that there is a match of integrated volumes shifted by half a timestep.
     # the integration of flux on the Ribasim side deserves a closer look
