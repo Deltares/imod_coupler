535195e2f287d4de16d256c1408884f7b1fcbfdb
rleander <Robert.Leander@deltares.nl>
2024-09-19 11:35:27 +0200

    cleanup operation

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -8,23 +8,11 @@ import jetbrains.buildServer.configs.kotlin.Project
 object Project : Project({
     description = "Python scripts coupling components"
 
-    vcsRoot(ImodCouplerImod55)
-    vcsRoot(ImodCouplerImod54)
     vcsRoot(MetaSwapLookupTable)
     vcsRoot(ImodCoupler)
-    vcsRoot(ImodCouplerImod56)
 
-    buildType(TestbenchCouplerWin64Lumbricus)
-    buildType(TestbenchCouplerWin64Release53)
-    buildType(TestbenchCouplerWin64Release54)
-    buildType(TestbenchCouplerWin64Release52)
     buildType(TestbenchCouplerWin64_2)
-    buildType(TestbenchCouplerWin64Release55)
-    buildType(TestbenchCouplerWin64Release56)
     buildType(MakeGitHubRelease)
-
-    template(Linux)
-    template(Windows)
 
     subProject(Primod.Project)
     subProject(IMODCollector.Project)
