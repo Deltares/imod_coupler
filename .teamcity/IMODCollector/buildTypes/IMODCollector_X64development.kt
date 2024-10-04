e9a32e24f750cf5f94d1e35400e3bb1be4e84f8c
rleander <Robert.Leander@deltares.nl>
2024-10-04 11:53:08 +0200

    temporary use another build of ribasim windows with the specific branch that has new added solvers

--- .teamcity/IMODCollector/buildTypes/IMODCollector_X64development.kt
+++ .teamcity/IMODCollector/buildTypes/IMODCollector_X64development.kt
@@ -107,7 +107,7 @@ object IMODCollector_X64development : BuildType({
             }
         }
         artifacts(AbsoluteId("Ribasim_Windows_BuildRibasim")) {
-            buildRule = lastSuccessful()
+            buildRule = lastSuccessful(branch="reltolerance")
             artifactRules = "ribasim_windows.zip!** => ribasim"
         }
     }
