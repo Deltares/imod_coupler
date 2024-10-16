66f40fd00942f707dc04c8bddc0eff5d62c80fd8
rleander <Robert.Leander@deltares.nl>
2024-10-16 10:00:06 +0200

    cleanup operation (#330)
    * cleanup operation

--- .teamcity/IMODCollector/Project.kt
+++ .teamcity/IMODCollector/Project.kt
@@ -1,7 +1,6 @@
 package IMODCollector
 
 import IMODCollector.buildTypes.*
-import IMODCollector.vcsRoots.*
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.Project
 
@@ -10,17 +9,7 @@ object Project : Project({
     name = "iMOD_Collector"
     description = "Collect iMOD6 coupled components + coupler into a single package"
 
-    vcsRoot(IMODCollector_ImodCouplerReleaseImod52)
-    vcsRoot(IMODCollector_ImodCouplerReleaseImod53)
-
-    buildType(IMODCollector_X64Release561)
+    buildType(IMODCollector_X64Release)
     buildType(IMODCollector_X64development)
-    buildType(IMODCollector_X64Release55)
-    buildType(IMODCollector_X64Release54)
-    buildType(IMODCollector_X64Release53)
-    buildType(IMODCollector_X64Release52)
     buildType(IMODCollector_X64developmentLnx64)
-    buildType(IMODCollector_X64lumbricus)
-    buildType(IMODCollector_X64Release561test)
-    buildType(IMODCollector_X64Release56)
 })
