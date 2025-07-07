dd66f75beb9c8493f701b766d2044364eb7ccd43
rleander <Robert.Leander@deltares.nl>
2025-07-07 14:13:41 +0200

    disable experimental feature concentration on the ribasim test model, leads to negative mass error in the coupler tests

--- tests/fixtures/fixture_ribasim.py
+++ tests/fixtures/fixture_ribasim.py
@@ -5,9 +5,10 @@ import pandas as pd
 import pytest_cases
 import ribasim
 import ribasim_testmodels
+from ribasim.config import Experimental
 
 solver_algorithm: str = "QNDF"
-
+experimental = Experimental(concentration=False, allocation=True)
 
 def add_subgrid(model: ribasim.Model) -> ribasim.Model:
     """Add 1:1 subgrid levels to model"""
@@ -36,6 +37,7 @@ def ribasim_bucket_model() -> ribasim.Model:
     bucket = ribasim_testmodels.bucket_model()
     bucket.endtime = datetime(2023, 1, 1, 0, 0)
     bucket.solver.algorithm = solver_algorithm
+    bucket.experimental=experimental
     return add_subgrid(bucket)
 
 
@@ -44,6 +46,7 @@ def ribasim_bucket_model_no_subgrid() -> ribasim.Model:
     bucket = ribasim_testmodels.bucket_model()
     bucket.endtime = datetime(2023, 1, 1, 0, 0)
     bucket.solver.algorithm = solver_algorithm
+    bucket.experimental=experimental
     return bucket
 
 
@@ -51,6 +54,7 @@ def ribasim_bucket_model_no_subgrid() -> ribasim.Model:
 def ribasim_backwater_model() -> ribasim.Model:
     backwater = ribasim_testmodels.backwater_model()
     backwater.solver.algorithm = solver_algorithm
+    backwater.experimental=experimental
     return add_subgrid(backwater)
 
 
@@ -58,6 +62,7 @@ def ribasim_backwater_model() -> ribasim.Model:
 def ribasim_two_basin_model() -> ribasim.Model:
     twobasin = ribasim_testmodels.two_basin_model()
     twobasin.solver.algorithm = solver_algorithm
+    twobasin.experimental = experimental
     return add_subgrid(twobasin)
 
 
@@ -67,4 +72,5 @@ def ribasim_two_basin_model_dbg() -> ribasim.Model:
     #   model.logging.verbosity = ribasim.Verbosity("debug")
     #   model.logging.verbosity = "debug"
     model.solver.algorithm = solver_algorithm
+    model.experimental=experimental
     return model
