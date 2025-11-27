f4074bf7649d28e7abb6dddb52ecdbee6566923e
rleander <Robert.Leander@deltares.nl>
2025-11-27 12:01:33 +0100

    upgrade modflow6 to 6.6.2

--- .teamcity/IMODCollector/buildTypes/IMODCollector_X64development.kt
+++ .teamcity/IMODCollector/buildTypes/IMODCollector_X64development.kt
@@ -33,8 +33,8 @@ object IMODCollector_X64development : BuildType({
             name = "Download Release MODFLOW 6.5.0"
             scriptContent = """
                 mkdir modflow6
-                curl -O https://water.usgs.gov/water-resources/software/MODFLOW-6/mf6.6.3_win64.zip
-                unzip  -j "mf6.6.3_win64.zip" -d modflow6 mf6.6.3_win64/bin/libmf6.dll
+                curl -O https://water.usgs.gov/water-resources/software/MODFLOW-6/mf6.6.2_win64.zip
+                unzip  -j "mf6.6.2_win64.zip" -d modflow6 mf6.6.2_win64/bin/libmf6.dll
             """.trimIndent()
         }
         script {
