6c06fd6afad7aeba18e97c69d29e1239f8f501c1
Sunny Titus <77051845+Manangka@users.noreply.github.com>
2026-01-15 16:43:39 +0100

    Add a buildstep to download existing imod_coupler releases (#414)

---
+++ .teamcity/IMODCollector/buildTypes/CouplerRegressionBinaries.kt
@@ -0,0 +1,37 @@
+package IMODCollector.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+object Coupler_Regression_Binaries : BuildType( {
+    name = "Coupler regression binaries"
+    description = "Download Coupler release binaries"
+
+    params {
+        select("COUPLER_Platform", "",
+            options = listOf("windows"))
+        select("COUPLER_Version", "",
+            options = listOf("v2025.11.0", "v2024.3.0", "v2024.2.0", "v2024.01.2"))
+    }
+
+    artifactRules = """
+        imod_coupler/** => imod_coupler_release.zip!
+    """.trimIndent()
+
+    vcs {
+    }
+
+    steps {
+        script {
+            name = "Download iMOD Coupler"
+            scriptContent = """
+                wget https://github.com/Deltares/imod_coupler/releases/download/%COUPLER_Version%/imod_coupler_%COUPLER_Platform%.zip -O imod_coupler.zip
+                unzip  "imod_coupler.zip"
+            """.trimIndent()
+        }
+    }
+
+    requirements {
+        equals("teamcity.agent.jvm.os.name", "Linux")
+    }
+})
