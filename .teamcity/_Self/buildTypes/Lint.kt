d9b89e236d20143eac0c0d751a03a2cfbba8b92f
Sunny Titus <sunny.titus@deltares.nl>
2026-01-15 14:52:22 +0100

    Move the lint CI step to TeamCity

---
+++ .teamcity/_Self/buildTypes/Lint.kt
@@ -0,0 +1,34 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+
+object Lint : BuildType({
+    name = "Lint"
+
+    vcs {
+        root(_Self.vcsRoots.ImodCoupler, ". => imod_coupler")
+        cleanCheckout = true
+    }
+
+    steps {
+        script {
+            name = "Run ruff format"
+            id = "Run_ruff_format"
+            workingDir = "imod-python"
+            scriptContent = """
+                    pixi run --environment default --frozen format-check 
+                """.trimIndent()
+            formatStderrAsError = true
+        }
+        script {
+            name = "Run ruff"
+            id = "Run_ruff"
+            workingDir = "imod-python"
+            scriptContent = """
+                    pixi run --environment default --frozen ruff
+                """.trimIndent()
+            formatStderrAsError = true
+        }
+    }
+})
