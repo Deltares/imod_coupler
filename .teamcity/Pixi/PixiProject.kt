9dce274b130396033085061c1435f1dc5fbbd22f
Sunny Titus <sunny.titus@deltares.nl>
2026-01-19 11:45:45 +0100

    Add pixi dependencies update build. Remove old Github action

---
+++ .teamcity/Pixi/PixiProject.kt
@@ -0,0 +1,91 @@
+package Pixi
+
+import _Self.vcsRoots.ImodCoupler
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.triggers.schedule
+
+object PixiProject : Project({
+    name = "Pixi"
+
+    buildType(UpdateDependencies)
+})
+
+object UpdateDependencies : BuildType({
+    name = "Update Dependencies"
+
+    params {
+        param("GH_USER", "deltares-service-account")
+        text("env.GH_TOKEN", "%github_deltares-service-account_access_token%")
+    }
+
+    vcs {
+        root(ImodCoupler, ". => imod_coupler")
+        cleanCheckout = true
+    }
+
+    steps {
+        powerShell {
+            name = "Update dependencies"
+            id = "Update_dependencies"
+            workingDir = "imod-python"
+            scriptMode = script {
+                content = """
+                    echo "Create update branch"
+                    git remote set-url origin https://%GH_USER%:%env.GH_TOKEN%@github.com/Deltares/imod-coupler.git
+                    git checkout -b pixi_update_%build.counter%
+                    
+                    echo "Update dependencies" 
+                    pixi run -e pixi-update update
+                    
+                    echo "Add any changes" 
+                    git add pixi.lock
+                    
+                    if (git status -suno pixi.lock) 
+                    {
+                      echo "Setup username and email"
+                      git config --global user.name "Teamcity"
+                      git config --global user.email "teamcity@deltares.nl"
+                      
+                      echo "Commit changes"
+                      git commit -m "Update pixi.lock"
+
+                      echo "Push changes" 
+                      git push -u origin pixi_update_%build.counter%
+
+                      echo "Format PR body"
+                      ${'$'}diff = Get-Content -Path diff.md
+                      Set-Content body.md 'Teamcity automatically updated the dependencies defined the pixi.toml file. Please verify that all tests succeed before merging'
+                      Add-Content -Path body.md -Value "`r`n"
+                      Add-Content -Path body.md -Value ${'$'}diff
+                      
+                      echo "Create PR"
+                      pixi run --environment default gh pr create --title "[TEAMCITY] Update project dependencies" --body-file body.md --reviewer rleander73,HendrikKok,Manangka
+                      echo "Changes pushed and PR created"
+                    }
+                    else
+                    {
+                      echo "No changes found"
+                    }
+                """.trimIndent()
+            }
+            noProfile = false
+        }
+    }
+
+    triggers {
+        schedule {
+            schedulingPolicy = weekly {
+                hour = 14
+            }
+            branchFilter = "+:<default>"
+            triggerBuild = always()
+            withPendingChangesOnly = false
+        }
+    }
+
+    failureConditions {
+        errorMessage = true
+    }
+})
