7050f0a56d00568d5ff3dbd15b364458643b9b89
Sunny Titus <77051845+Manangka@users.noreply.github.com>
2026-03-27 21:29:21 +0100

    Align the CI pipeline with the way metaswap is build locally using pixi (#441)

--- .teamcity/IMODCollector/Project.kt
+++ .teamcity/IMODCollector/Project.kt
@@ -2,7 +2,6 @@ package IMODCollector
 
 import IMODCollector.buildTypes.Coupler_Regression_Binaries
 import IMODCollector.buildTypes.IMODCollector_X64development
-import IMODCollector.buildTypes.Ribasim_binaries
 import jetbrains.buildServer.configs.kotlin.Project
 
 object Project : Project({
@@ -11,7 +10,6 @@ object Project : Project({
     description = "Collect iMOD6 coupled components + coupler into a single package"
 
     buildType(IMODCollector_X64development)
-    buildType(Ribasim_binaries)
     buildType(Coupler_Regression_Binaries)
 })
 
