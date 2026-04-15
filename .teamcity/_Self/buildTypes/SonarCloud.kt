aa49fa943c20951069d91d81851ebb966df945f5
Sunny Titus <sunny.titus@deltares.nl>
2026-04-15 11:52:10 +0200

    Add a sonarqube buildstep to the TeamCity pipeline

---
+++ .teamcity/_Self/buildTypes/SonarCloud.kt
@@ -0,0 +1,35 @@
+package _Self.buildTypes
+
+import Templates.GitHubIntegrationTemplate
+import _Self.vcsRoots.ImodCoupler
+import jetbrains.buildServer.configs.kotlin.*
+
+object SonarCloud : BuildType({
+    name = "SonarCloud"
+    description = "Runs SonarCloud static analysis"
+
+    templates(GitHubIntegrationTemplate)
+
+    params {
+        password("sonar_token", "%sonar_token%")
+    }
+
+    vcs {
+        root(ImodCoupler, ". => imod_coupler")
+        cleanCheckout = true
+    }
+
+    steps {
+        step {
+            name = "SonarCloud analysis"
+            id = "Sonar_analysis"
+            type = "sonar-plugin"
+            param("teamcity.build.workingDir", "imod_coupler")
+            param("sonarServer", "54d6c253-800e-4025-870b-cb760324147b")
+        }
+    }
+
+    requirements {
+        equals("env.OS", "Windows_NT")
+    }
+})
