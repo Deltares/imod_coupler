771a809dcf6c8a5e5e8b00a2df57edfcdf05d8fc
verkaik <jarno.verkaik@deltares.nl>
2026-07-14 08:38:44 +0200

    Update test.

--- tests/test_imod_coupler/test_metamod.py
+++ tests/test_imod_coupler/test_metamod.py
@@ -520,7 +520,7 @@ def add_logging_request_to_toml_file(toml_dir: Path, toml_filename: str) -> None
         toml_dict = tomli.load(f)
 
     with open(toml_dir / toml_filename, "wb") as f:
-        toml_dict["driver"]["coupling"][0]["output_config_file"] = (
+        toml_dict["driver"]["coupling"]["output_config_file"] = (
             "./output_config.toml"
         )
         tomli_w.dump(toml_dict, f)
